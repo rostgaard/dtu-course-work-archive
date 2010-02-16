2f1bfec48f3c8fc4497e4ca1f69e15716adb8984
Hjallgrim Gunnar Mohr Hentze <s062418@student.dtu.dk>
2010-02-16 14:11:16 +0000

    Working properly according to the new specs. Ignore the last log msg.
    git-svn-id: file:///home/krc/oldserver/lucretia.greenpc.dk/var/www/greenpc.dk/svn/02333@7 02983c5c-57ec-45de-9fea-5559225ce2e1

--- task3/src/kernel/scheduler.c
+++ task3/src/kernel/scheduler.c
@@ -5,34 +5,30 @@
 #include "threadqueue.h"
 
 
-if(schedule == 1){
-    register int  new_thread_index, old_thread_index;
+register int  new_thread_index, old_thread_index;
 
-    // Remove the head from the queue
-    new_thread_index = thread_queue_dequeue(&ready_queue);
-    // Save the current running thread
-    old_thread_index = cpu_private_data.thread_index;
+// Remove the head from the queue
+new_thread_index = thread_queue_dequeue(&ready_queue);
+// Save the current running thread
+old_thread_index = cpu_private_data.thread_index;
 
-    // Check if the running thread was blocked
-    if(thread_table[old_thread_index].data.list_data == 0){
-/*
-        kprints("Thread id: ");
-        kprinthex(old_thread_index);
-        kprints(" is now in ready queue\n");
-*/
-        // Add thread to queue if not blocked
-        thread_queue_enqueue(&ready_queue, old_thread_index);
-    }
+if(schedule != 1){
 
-    
-    
+    // Add thread to queue if not blocked
+    thread_queue_enqueue(&ready_queue, old_thread_index);
+
 /*
     kprints("Thread id: ");
-    kprinthex(new_thread_index);
-    kprints(" is now running\n");
+    kprinthex(old_thread_index);
+    kprints(" is now in ready queue\n");
+*/
+}
+
+/*
+kprints("Thread id: ");
+kprinthex(new_thread_index);
+kprints(" is now running\n");
 */
 
-    // Let the new thread run.
-    cpu_private_data.thread_index = new_thread_index;
-
-}
+// Let the new thread run.
+cpu_private_data.thread_index = new_thread_index;
