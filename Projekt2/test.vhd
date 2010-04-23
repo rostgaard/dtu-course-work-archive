591ff14b555ba1a72c604c14852515704f13f117
Kim Rostgaard Christensen <krc@retrospekt.dk>
2010-04-23 13:28:02 +0000

    git-svn-id: file:///home/krc/oldserver/lucretia.greenpc.dk/var/www/greenpc.dk/svn/02311@37 6e3375e2-8dbc-428e-bc8d-efee3975df3f

---
+++ Projekt2/test.vhd
@@ -0,0 +1,90 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:50:00 04/23/2010 
+-- Design Name: 
+-- Module Name:    test - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity test is port (
+	kr1 : in  STD_LOGIC;
+	kr2 : in  STD_LOGIC;
+	clk : in  STD_LOGIC;
+	amount : out STD_LOGIC_VECTOR(3 downto 0));
+end test;
+
+architecture Behavioral of test is
+type StateType is (init, purchase, add1, add2);
+signal state_reg, state_next : StateType := init;
+begin
+
+NextStateDecoding : process (state_reg)
+begin
+	--Init values
+	state_next <= state_reg;
+	--Assignments
+		case state_reg is
+			when init => 
+				state_next <= purchase;
+			when purchase =>
+				if(kr1 = '1') then 
+					state_next <= add1;
+				elsif (kr2 = '1') then 
+					state_next <= add2;
+				end if;
+			when add1 =>
+				state_next <= purchase;
+			when add2 =>
+				state_next <= purchase;
+			when others => 
+				state_next <= init;
+		end case;
+	end process NextStateDecoding;
+
+StateRegister: process (clk)
+begin
+	if clk'event and clk = '1' then
+		state_reg <= state_next;
+	end if;
+end process;
+
+	OutputDecoding : process (state_reg)
+	begin
+		case state_reg is
+			when init => 
+				amount <= "0001";
+
+			when purchase =>
+				amount <= "0010";
+			when add1 =>
+				amount <= "0011";
+			when add2 =>
+				amount <= "0100";
+			when others => 
+				amount <= "0000";
+		end case;
+	end process OutputDecoding;
+--Initial
+
+end Behavioral;
---
+++ Projekt2/test_tb.vhd
@@ -0,0 +1,105 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   15:09:36 04/23/2010
+-- Design Name:   
+-- Module Name:   C:/Documents and Settings/Administrator.PCV/Desktop/Digitalteknik/Projekt_1/project2/test_tb.vhd
+-- Project Name:  project2
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: test
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.all;
+USE ieee.numeric_std.ALL;
+ 
+ENTITY test_tb IS
+END test_tb;
+ 
+ARCHITECTURE behavior OF test_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT test
+    PORT(
+         kr1 : IN  std_logic;
+         kr2 : IN  std_logic;
+         clk : IN  std_logic;
+         amount : OUT  std_logic_vector(3 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal kr1 : std_logic := '0';
+   signal kr2 : std_logic := '0';
+   signal clk : std_logic := '0';
+
+ 	--Outputs
+   signal amount : std_logic_vector(3 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 1us;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: test PORT MAP (
+          kr1 => kr1,
+          kr2 => kr2,
+          clk => clk,
+          amount => amount
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100ms.
+
+      wait for clk_period*2;
+		kr1 <= '1';
+		wait for clk_period*2;
+		kr1 <= '0';
+		wait for clk_period*2;
+
+		kr2 <= '1';
+		wait for clk_period*2;
+		kr2 <= '0';
+		wait for clk_period*2;
+
+
+
+      -- insert stimulus here 
+    assert false
+      report "End of Simulation"
+      severity failure;
+   end process;
+
+END;
