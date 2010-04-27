9b6fffd54ba919268b57499e0105beb0c1b73509
Kim Rostgaard Christensen <krc@retrospekt.dk>
2010-04-27 13:49:42 +0000

    small bugfixes, and added ucf file
    git-svn-id: file:///home/krc/oldserver/lucretia.greenpc.dk/var/www/greenpc.dk/svn/02311@46 6e3375e2-8dbc-428e-bc8d-efee3975df3f

--- Projekt2/disp_hex_mux.vhd
+++ Projekt2/disp_hex_mux.vhd
@@ -7,8 +7,8 @@ entity disp_hex_mux is
     clk, reset             : in  std_logic;
     hex3, hex2, hex1, hex0 : in  std_logic_vector(3 downto 0);
     dp_in                  : in  std_logic_vector(3 downto 0);
-    an                     : out std_logic_vector(3 downto 0);
-    sseg                   : out std_logic_vector(7 downto 0)
+    AN                     : out std_logic_vector(3 downto 0);
+    SEG                   : out std_logic_vector(7 downto 0)
     );
 end disp_hex_mux;
 
@@ -16,7 +16,7 @@ architecture arch of disp_hex_mux is
   constant N : integer := 18;
   signal q_reg, q_next : unsigned(N-1 downto 0);
   signal sel : std_logic_vector(1 downto 0);
-  signal hex : std_logic_vector(3 downto);
+  signal hex : std_logic_vector(3 downto 0);
   signal dp : std_logic;
 
 begin
@@ -37,24 +37,24 @@ begin
   q_next <= q_reg + 1 ;
 
   -- 2 MSBs of counter to control 4-to-1 multiplexing
-  sel <= std_logic_vector(qreg(N-1 downto N-2));
+  sel <= std_logic_vector(q_reg(N-1 downto N-2));
   process (sel,hex0,hex1,hex2,hex3,dp_in)
   begin  -- process
     case sel is
       when "00" =>
-        an <= "1110";
+        AN <= "1110";
         hex <= hex0;
         dp <= dp_in(0);
       when "01" =>
-        an <= "1101";
+        AN <= "1101";
         hex <= hex1;
         dp <= dp_in(1);
       when "10" =>
-        an <= "1011";
+        AN <= "1011";
         hex <= hex2;
         dp <= dp_in(2);
       when "11" =>
-        an <= "0111";
+        AN <= "0111";
         hex <= hex3;
         dp <= dp_in(3);
 
@@ -63,10 +63,10 @@ begin
   end process;
 
   with hex select
-    sseg(6 downto 0) <=
+    SEG(6 downto 0) <=
     "0000001" when "0000",
     "1001111" when "0001",
-    "0010010" when "0010"
+    "0010010" when "0010",
     "0000110" when "0011",
     "1001100" when "0100",
     "0100100" when "0101",
@@ -81,5 +81,5 @@ begin
     "0110000" when "1110", --e    
     "0111000" when others; --f
   -- decimal point
-  sseg(7) <= dp;
+  SEG(7) <= dp;
 end arch;
--- Projekt2/hex_mux_test.vhd
+++ Projekt2/hex_mux_test.vhd
@@ -6,8 +6,8 @@ entity hex_mux_test is
   port(
     clk: in std_logic;
     sw: in std_logic_vector ( 7 downto 0 ) ;
-    an: out std_logic_vector (3 downto 0 ) ;
-    sseg : out std_logic_vector (7 downto 0 ));
+    AN: out std_logic_vector (3 downto 0 ) ;
+    SEG : out std_logic_vector (7 downto 0 ));
 end hex_mux_test;
 
 architecture arch of hex_mux_test is
@@ -17,14 +17,14 @@ begin
   disp_unit : entity work.disp_hex_mux
     port map(
       clk=>clk,
-      reset=>'O',
+      reset=>'0',
       hex3=>sum(7 downto 4),
       hex2=>sum(3 downto 0),
-      hexl=>sw(7 downto 4),
-      hexO=>sw(3 downto 0),
-      dp_in=>"lOll",
-      an=>an,
-      sseg=>sseg);
+      hex1=>sw(7 downto 4),
+      hex0=>sw(3 downto 0),
+      dp_in=>"1011",
+      AN=>AN,
+      SEG=>SEG);
   
   a   <= "0000" & unsigned(sw(3 downto 0));
   b   <= "0000" & unsigned(sw(7 downto 4));
