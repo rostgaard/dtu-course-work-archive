d876fb4bf69d58eede20ac5e28562ffc90d0ae32
Kim Rostgaard Christensen <krc@retrospekt.dk>
2010-10-11 13:31:26 +0000

    Makefile and test case
    git-svn-id: file:///home/krc/oldserver/lucretia.greenpc.dk/var/www/greenpc.dk/svn/02332@13 a0350fe0-df2d-4aa2-9bf3-cd012c838ce5

--- MiniJava/src/compiler/Frontend/test.java
+++ MiniJava/src/compiler/Frontend/test.java
@@ -1,17 +1,36 @@
-package compiler.Frontend;
+class mainClass {
 
-public class test {
-
+    public static void main(String[] args) {
 	int i;
+	test2 t2;
+	t2 = new test2();
+	t2.test(2);
+    }
+}
+
+class test2 {
+    String hej = "Hej";
+    int i = 0;
+
+    public test2() {
+    }
+
+    public String getString() {
+	return hej;
+    }
 	
-	test(int i) {
-		this.i = i;
-	}
+    public void test(int i) {
+	this.i = i;
+    }
 	
-	void x() {
-		int i;
-		{
-			
-		}
+    public void x() {
+	int i = 0;
+	if(i == 0) {
+	    i= 2;
 	}
+	else {
+	    i=3;
+	}
+    }
+    
 }
