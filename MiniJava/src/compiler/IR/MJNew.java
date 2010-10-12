1708ca0dd5fce3bf4ac9cc355fd6bd0e1044a172
Mikkel Kragelund Nielsen <s083325@student.dtu.dk>
2010-10-12 14:59:05 +0000

    git-svn-id: file:///home/krc/oldserver/lucretia.greenpc.dk/var/www/greenpc.dk/svn/02332@17 a0350fe0-df2d-4aa2-9bf3-cd012c838ce5

--- MiniJava/src/compiler/IR/MJNew.java
+++ MiniJava/src/compiler/IR/MJNew.java
@@ -4,7 +4,15 @@ import compiler.PrettyPrinter;
 
 public class MJNew extends MJExpression {
 
+	MJType type;
+
+	MJNew (MJType type){
+		this.type = type;
+		
+	}
 	public void prettyPrint(PrettyPrinter prepri) {
+		prepri.print("new");
+		type.prettyPrint(prepri);
 	}
 
 }
