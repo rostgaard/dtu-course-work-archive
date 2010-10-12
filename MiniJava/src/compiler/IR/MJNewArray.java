48d8ecb67658f571d175c87b341b11b823cf801d
Mikkel Kragelund Nielsen <s083325@student.dtu.dk>
2010-10-12 14:59:14 +0000

    git-svn-id: file:///home/krc/oldserver/lucretia.greenpc.dk/var/www/greenpc.dk/svn/02332@18 a0350fe0-df2d-4aa2-9bf3-cd012c838ce5

--- MiniJava/src/compiler/IR/MJNewArray.java
+++ MiniJava/src/compiler/IR/MJNewArray.java
@@ -4,7 +4,19 @@ import compiler.PrettyPrinter;
 
 public class MJNewArray extends MJNew {
 
+	MJExpression idx;
+	
+	
+	public MJNewArray(MJType type, MJExpression idx) {
+		super(type);
+		this.idx = idx;
+	}
+
 	public void prettyPrint(PrettyPrinter prepri) {
+		super.prettyPrint(prepri);
+		prepri.print("[");
+		idx.prettyPrint(prepri);
+		prepri.print("]");
 	}
 
 }
