993bd23e841584e57912ee0c0d681ba92333226e
Kim Rostgaard Christensen <krc@retrospekt.dk>
2011-01-07 11:26:48 +0000

    Before LunchyWonchy
    git-svn-id: file:///home/krc/oldserver/lucretia.greenpc.dk/var/www/greenpc.dk/svn/02321@33 857d0c8e-2298-4a52-90d3-44ad14c7eb9b

--- 3WeeksProject/LC3System/lc3_mem.vhd
+++ 3WeeksProject/LC3System/lc3_mem.vhd
@@ -13,7 +13,7 @@ entity lc3_mem is
     clk: in std_logic;
     we, re: in std_logic;
     addr: in std_logic_vector(ADDR_WIDTH-1 downto 0);
-    data: inout std_logic_vector(DATA_WIDTH-1 downto 0);
+    data: inout std_logic_vector(DATA_WIDTH-1 downto 0)
     );
 end lc3_mem;
 
@@ -22,44 +22,42 @@ architecture beh_arch of lc3_mem is
     of std_logic_vector (DATA_WIDTH-1 downto 0);
   signal ram: ram_type;
   signal addr_reg: std_logic_vector(ADDR_WIDTH-1 downto 0);
-  
   signal data_out: std_logic_vector(ADDR_WIDTH-1 downto 0);
   signal memory_signal: std_logic_vector(ADDR_WIDTH-1 downto 0);
   signal memory_map: std_logic_vector(ADDR_WIDTH-1 downto 0); 
-  
   signal cs: std_logic;
+  
 begin
+  
   process (clk)
   begin
     if (clk'event and clk = '1') then
       if (we='1') then
-        ram(to_integer(unsigned(addr))) <= din;
+        ram(to_integer(unsigned(addr))) <= data;
       end if;
       addr_reg <= addr;
     end if;
   end process;
   
-  data_out <= ram(to_integer(unsigned(addr_reg))) when cs = '1' else
-              memory_map;
+  data_out <= ram(to_integer(unsigned(addr_reg))) when cs = '1' else memory_map;
   
   -- Tri state buffer asserts read enable
-  memory_signal <=  ram(to_integer(unsigned(addr_reg))) when re = '1'
-                    else (others => 'Z');
+  memory_signal <=  ram(to_integer(unsigned(addr_reg))) when re = '1' else (others => 'Z');
   ram(to_integer(unsigned(addr_reg))) <= memory_signal;
 
   -- Chip selector
   cs <= '0' when addr(15 downto 8) = X"FE" else '1';
-
   
   data <= memory_signal when cs = '1' else memory_map;
+  
+  
 
   -- TODO routing for memory map with tri-state buffer in front
-  memory_map <= mmap_route when re = '1'
-                    else (others => 'Z');
+  memory_map <= mmap_route when re = '1' else (others => 'Z');
 
   mmap_route <= memory_map;
 
-  -- xFE00 Stdin Status Register
+  --   Stdin Status Register
   mmap_route <= uart_rx_status_reg when addr(7 downto 0) = X"00" else
   -- xFE02 Stdin Data Register
                 uart_rx_data_reg when addr(7 downto 0) = X"02" else
