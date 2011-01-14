6ee4c0eae668347a8f06597f9c5d1e05cbf877a3
Kim Rostgaard Christensen <krc@retrospekt.dk>
2011-01-14 09:35:20 +0000

    git-svn-id: file:///home/krc/oldserver/lucretia.greenpc.dk/var/www/greenpc.dk/svn/02321@45 857d0c8e-2298-4a52-90d3-44ad14c7eb9b

---
+++ 3WeeksProject/delay.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main() {
+  unsigned short lsb = 0;
+  unsigned short msb = 0;
+
+
+  while (1) {
+  while (msb != 0x05f5u) {
+    lsb = 0;
+    while (lsb != 0xffffu) {
+      lsb++;
+
+    }
+    msb++;
+  }
+  printf("%x%x \n",msb,lsb);
+
+  msb=0;
+
+  }
+}
+
+  
