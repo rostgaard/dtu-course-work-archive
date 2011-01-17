67b7c9708d607b4dd732249de3550d73980dfdde
Kim Rostgaard Christensen <krc@retrospekt.dk>
2011-01-17 14:34:41 +0000

    Virker på LC3
    git-svn-id: file:///home/krc/oldserver/lucretia.greenpc.dk/var/www/greenpc.dk/svn/02321@52 857d0c8e-2298-4a52-90d3-44ad14c7eb9b

--- RandomTest.c
+++ RandomTest.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
-short next = 12323;
+short next = 123;
 
-int main(void) {
+short rand(short limit)
+{
+	short i;
+	next = next * 13 + 7;
+	i = (next/63) % limit;
+	//printf("after limit: %d, next : %d i: %d; mod: %d\n", limit, next, i, i % limit);
+	return i;
+}
 
-	//srand(12323);
-	rand();
-	printf("Random value: %d",next);
+
+int main(void) 
+{
+	short limit = 80;
+	short result = rand(limit);
+	printf("Random value: %d", result);
 	return 0;
-}
-
-short rand(void)
-{
-	next = next * 11034 + 135;
-	return (short)(next/636)%328;
-}
-
-void srand(short seed)
-{
-	next = seed;
 }
