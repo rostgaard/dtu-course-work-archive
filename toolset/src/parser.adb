2290e88373585b89d8b13ec2d46f66cb8a6f6bd0
Kim Rostgaard Christensen <krc@retrospekt.dk>
2013-04-30 12:41:30 +0200

    Tidied up a bit.

--- toolset/src/parser.adb
+++ toolset/src/parser.adb
@@ -23,25 +23,6 @@ procedure Parser is
       return Buf;
    end Image;
 
-   type Rail_Kinds is (Undefined, Link, Switchtrack, Station, End_Point);
-
-   type Rails (Kind : Rail_Kinds := Undefined) is
-      record
-         case Kind is
-            when Undefined =>
-               null;
-            when Link =>
-               Identifier : Identifiers     := Null_Identifier;
-               Link1      : Identifications := Null_Identification;
-               Link2      : Identifications := Null_Identification;
-            when others =>
-               null;
-         end case;
-      end record
-   with Type_Invariant => not Link2 = End_Point and
-     (Link1 = End_Point and Link2 /= Null_Identification);
-
-
    function Create (Identifier : in Identifiers) return Rails is
    begin
       return (Defined    => True,
--- toolset/src/railval-parser.ads
+++ toolset/src/railval-parser.ads
@@ -1,3 +1,22 @@
 package Railval.Parser is
 
+
+   type Rail_Kinds is (Undefined, Link, Switchtrack, Station, End_Point);
+
+   type Rails (Kind : Rail_Kinds := Undefined) is
+      record
+         case Kind is
+            when Undefined =>
+               null;
+            when Link =>
+               Identifier : Identifiers     := Null_Identifier;
+               Link1      : Identifications := Null_Identification;
+               Link2      : Identifications := Null_Identification;
+            when others =>
+               null;
+         end case;
+      end record
+   with Type_Invariant => not Link2 = End_Point and
+     (Link1 = End_Point and Link2 /= Null_Identification);
+
 end Railval.Parser;
