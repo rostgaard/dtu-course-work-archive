5884a4276164c79d353b3417118cd7d0468224be
Kim Rostgaard Christensen <krc@retrospekt.dk>
2013-05-22 15:43:11 +0200

    This is acutally obsolecent, but needed to make a complete compile.

---
+++ src/obsolecent/Protocol.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obsolecent;
+
+/**
+ *
+ * @author krc
+ */
+class Protocol {
+
+    public static final String registryAcknowledgement = "RAK";
+    public static final String registrySearch = "RSE";
+}
