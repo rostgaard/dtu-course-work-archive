3654037cf34783266016564d2fe3bd5291e5106e
Thomas Pedersen <s103665@student.dtu.dk>
2013-06-17 14:05:14 +0200

    Found this awesome MD5 example

--- project2/src/main/Program.java
+++ project2/src/main/Program.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Program {
 
 	/**
@@ -8,6 +11,37 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub, hest
 		System.out.println("Hello World!");
+		
+		try {
+			MD5();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
 	}
 
+	static void MD5() throws NoSuchAlgorithmException{
+		String password = "123456";
+		 
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+ 
+        byte byteData[] = md.digest();
+ 
+        //convert the byte to hex format method 1
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+         sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+ 
+        System.out.println("Digest(in hex format):: " + sb.toString());
+ 
+        //convert the byte to hex format method 2
+        StringBuffer hexString = new StringBuffer();
+    	for (int i=0;i<byteData.length;i++) {
+    		String hex=Integer.toHexString(0xff & byteData[i]);
+   	     	if(hex.length()==1) hexString.append('0');
+   	     	hexString.append(hex);
+    	}
+    	System.out.println("Digest(in hex format):: " + hexString.toString());
+	}
 }
