23778d7c162c3df8db816993b57207455ae56a43
Lars Kristensen <kennykudda@gmail.com>
2013-06-17 14:01:56 +0200

    Added Hest
    Was important to me

--- project2/src/main/Program.java
+++ project2/src/main/Program.java
@@ -6,7 +6,7 @@ public class Program {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub, hest
 		System.out.println("Hello World!");
 	}
 
