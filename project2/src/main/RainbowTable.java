e7b9ffd10ca99a10f5af25cf587ed95da24bb76f
Kim Rostgaard Christensen <krc@retrospekt.dk>
2013-06-18 10:49:25 +0200

    Added rainbow table class.

---
+++ project2/src/main/RainbowTable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.main;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author krc
+ */
+public class RainbowTable extends HashMap<String, Long>{
+    
+}
