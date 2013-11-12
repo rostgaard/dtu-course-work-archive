306617714f334c455bfbfc46836074403b0638d3
Kim Rostgaard Christensen <krc@retrospekt.dk>
2013-11-12 09:10:01 +0100

    Forgot a file

---
+++ analysis_tool/src/flowgraph/datastructure/NodeSet.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package flowgraph.datastructure;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author krc
+ */
+public class NodeSet extends ArrayList<Node> {
+
+    public final static NodeSet emptySet = new NodeSet();
+
+    public NodeSet addNode(Node n) {
+        super.add(n);
+
+        return this;
+    }
+    
+    /**
+     * Horribly inefficient Union operation, please fix prior to actual release 
+     * upon the world.
+     * @param nset 
+     */
+    public NodeSet union (NodeSet nset) {
+        for (Node n : nset){
+            if (!this.contains(n)) {
+                this.add(n);
+            }
+        }
+        
+        return this;
+    }
+}
