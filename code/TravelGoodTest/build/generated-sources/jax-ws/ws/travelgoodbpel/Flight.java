24bac8792e9210810b3a803c5165debefdd10a37
Martin Tange <mktange@gmail.com>
2013-11-19 11:42:33 +0100

    LameDuck added to BPEL

---
+++ code/TravelGoodTest/build/generated-sources/jax-ws/ws/travelgoodbpel/Flight.java
@@ -0,0 +1,178 @@
+
+package ws.travelgoodbpel;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Java class for flight complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="flight">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="flightID" type="{http://travelgoodbpel.ws}flightID"/>
+ *         &lt;element name="departure" type="{http://www.w3.org/2001/XMLSchema}dateTime"/>
+ *         &lt;element name="arrival" type="{http://www.w3.org/2001/XMLSchema}dateTime"/>
+ *         &lt;element name="origin" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="destination" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "flight", propOrder = {
+    "flightID",
+    "departure",
+    "arrival",
+    "origin",
+    "destination"
+})
+public class Flight {
+
+    @XmlElement(required = true)
+    protected String flightID;
+    @XmlElement(required = true)
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar departure;
+    @XmlElement(required = true)
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar arrival;
+    @XmlElement(required = true)
+    protected String origin;
+    @XmlElement(required = true)
+    protected String destination;
+
+    /**
+     * Gets the value of the flightID property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFlightID() {
+        return flightID;
+    }
+
+    /**
+     * Sets the value of the flightID property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFlightID(String value) {
+        this.flightID = value;
+    }
+
+    /**
+     * Gets the value of the departure property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getDeparture() {
+        return departure;
+    }
+
+    /**
+     * Sets the value of the departure property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setDeparture(XMLGregorianCalendar value) {
+        this.departure = value;
+    }
+
+    /**
+     * Gets the value of the arrival property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getArrival() {
+        return arrival;
+    }
+
+    /**
+     * Sets the value of the arrival property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setArrival(XMLGregorianCalendar value) {
+        this.arrival = value;
+    }
+
+    /**
+     * Gets the value of the origin property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getOrigin() {
+        return origin;
+    }
+
+    /**
+     * Sets the value of the origin property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setOrigin(String value) {
+        this.origin = value;
+    }
+
+    /**
+     * Gets the value of the destination property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDestination() {
+        return destination;
+    }
+
+    /**
+     * Sets the value of the destination property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDestination(String value) {
+        this.destination = value;
+    }
+
+}
