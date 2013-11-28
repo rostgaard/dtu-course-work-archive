cd755ff3530ff05641e3cc445a6c8c33eb531e76
ibrahimnemli <ibrahimnemli@msn.com>
2013-11-28 15:08:22 +0100

    New program slicing

--- analysis_tool/src/analysis/ProgramSlicing.java
+++ analysis_tool/src/analysis/ProgramSlicing.java
@@ -2,42 +2,86 @@ package analysis;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
-
-import flowgraph.datastructure.NodeSet;
 import org.apache.commons.collections15.map.HashedMap;
-
+import syntaxtree.StatementList;
+import syntaxtree.expression.Variable;
+import flowgraph.datastructure.NodeSet;
 import flowgraph.datastructure.Node;
 import flowgraph.datastructure.VariableSet;
 
 public class ProgramSlicing {
 
-	private HashedMap<Integer, ArrayList<Definition>> slices = new HashedMap<Integer, ArrayList<Definition>>();
-	
-	
-	public static NodeSet execute(Node poi, RDProgramState analysis){
-        System.out.println("POI: " + poi);
-        NodeSet result = new NodeSet();
-        LinkedList<Node> queue = new LinkedList<Node>();
+	public static void execute(StatementList stmts, LatticeSet lattice){
+		NodeSet labels = stmts.lables();
+		for(int i = 0; i<labels.size(); i++){
+			Node node = labels.get(i);
+			RDLattice rdlat = (RDLattice)lattice.get(node);
+			programSlice(node, rdlat);
+		}
+	}
 
-        HashedMap<Integer, Boolean> visitedNodes = new HashedMap<>();
+	public static void programSlice(Node poi, RDLattice analysis){
+		NodeSet result = new NodeSet();
+		LinkedList<Node> queue = new LinkedList<Node>();
+		HashedMap<Integer, Boolean> visitedNodes = new HashedMap<>();
+
+		queue.add(poi);
+		result.add(poi);
+		while (!queue.isEmpty()) {
+			Node label = queue.removeFirst();
+			VariableSet variables = label.getVariables();
+
+			ArrayList<Variable> vars = new ArrayList<>(analysis.keySet());
+			for(Variable variable: vars) {
+				DefinitionSet set = analysis.get(variable);
+				for(Definition def : set){
+					Boolean containsVariable = variables.contains(variable);
+					Boolean labelDefined = !analysis.get(variable).isEmpty();
+					if (containsVariable && labelDefined && !visitedNodes.containsKey(def.label.getLabel())) {
+						visitedNodes.put(def.label.getLabel(), true);
+						if(!result.contains(def.label)){
+							result.add(def.label);
+						}
+						queue.add(def.label);
+					
+					}
+				}
+			}
+		}
+		System.out.println("POI: " + poi.getLabel() + " result: " + result);		
+//		return result;
+	}
+
+
+	/**
+	 * @deprecated
+	 * @param poi
+	 * @param analysis
+	 * @return
+	 */
+	public static NodeSet execute(Node poi, RDProgramState analysis){
+		System.out.println("POI: " + poi);
+		NodeSet result = new NodeSet();
+		LinkedList<Node> queue = new LinkedList<Node>();
+
+		HashedMap<Integer, Boolean> visitedNodes = new HashedMap<>();
 		queue.add(poi);
 		while (!queue.isEmpty()) {
 			Node label = queue.removeFirst();
 			VariableSet variables = label.getVariables();
-            DefinitionSet definitions = analysis.getRDEntry(label.getLabel());
-            for(Definition definition : definitions) {
-                Boolean containsVariable = variables.contains(definition.identifier);
-                Boolean labelDefined = definition.label!=null;
+			DefinitionSet definitions = analysis.getRDEntry(label.getLabel());
+			for(Definition definition : definitions) {
+				Boolean containsVariable = variables.contains(definition.identifier);
+				Boolean labelDefined = definition.label!=null;
 
-                if (containsVariable && labelDefined && !visitedNodes.containsKey(definition.label.getLabel())) {
-                    visitedNodes.put(definition.label.getLabel(), true);
-                    result.add(definition.label);
-                    queue.add(definition.label);
-                }
-            }
+				if (containsVariable && labelDefined && !visitedNodes.containsKey(definition.label.getLabel())) {
+					visitedNodes.put(definition.label.getLabel(), true);
+					result.add(definition.label);
+					queue.add(definition.label);
+				}
+			}
 		}
 
-        return result;
+		return result;
 	}
 }
