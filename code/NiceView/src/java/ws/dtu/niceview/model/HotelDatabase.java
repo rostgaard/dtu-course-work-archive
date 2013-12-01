d1d829cb9daf1ef22772b74833a0c21deaafee81
Martin Tange <mktange@gmail.com>
2013-12-01 16:35:22 +0100

    Added NiceViewTest and updated NV/LD

--- code/NiceView/src/java/ws/dtu/niceview/model/HotelDatabase.java
+++ code/NiceView/src/java/ws/dtu/niceview/model/HotelDatabase.java
@@ -18,11 +18,10 @@ public class HotelDatabase {
     private Map<String, HotelList> cityMap = new HashMap<String, HotelList>();
     private Set<String> bookings = new HashSet<String>();
     private String serviceName = "NiceView";
-    
     private static HotelDatabase db;
     
     public static synchronized HotelDatabase getInstance() {
-        if(db == null){
+        if (db == null) {
             db = new HotelDatabase();
             db.loadDatabase();
         }
@@ -30,7 +29,7 @@ public class HotelDatabase {
         return db;
     }
     
-    private HotelDatabase(){
+    private HotelDatabase() {
         init();
     }
     
@@ -46,7 +45,7 @@ public class HotelDatabase {
         this.loadDatabase();
     }
     
-    private  void loadDatabase() {
+    private void loadDatabase() {
         add("Copenhagen", "Istedgade 1", "CPH1", true, "SAS Hotel", 800, "NiceView");
         add("Copenhagen", "Nørregade 21", "CPH2", true, "Radison Blue Hotel", 500, "NiceView");
         add("San Francisco", "Coolstreet 13", "KAB1", true, "Clear sky Hotel", 230, "NiceView");
@@ -86,24 +85,30 @@ public class HotelDatabase {
     }
     
     public HotelList getHotels(String city, XMLGregorianCalendar arrival, XMLGregorianCalendar departure) {
+        HotelList result = new HotelList();
+        
         if (cityMap.containsKey(city)) {
-             return cityMap.get(city);
-        }else {
-            return new HotelList();
+            for (HotelInformation hi : cityMap.get(city).getHotels()) {
+                if (!bookings.contains(hi.getBookingNo())) {
+                    result.getHotels().add(hi);
+                }
+            }
         }
+        
+        return result;
     }
     
     public HotelInformation getHotel(String bookingNo) {
-        HotelInformation hotel = (HotelInformation)bookingNoMap.get(bookingNo);
+        HotelInformation hotel = (HotelInformation) bookingNoMap.get(bookingNo);
         if (hotel == null) {
             // TODO: FAULT
         }
         return hotel;
     }
-
-    public HotelInformation bookHotel(String bookingNo, CreditCardInfoType ccInformation) throws BookHotelFault {
+    
+    public HotelInformation bookHotel(String bookingNo) throws BookHotelFault {
         if (!bookingNoMap.containsKey(bookingNo)) {
-            throw new BookHotelFault("No such booking number: "+bookingNo, "No such booking number: "+bookingNo);
+            throw new BookHotelFault("No such booking number: " + bookingNo, "No such booking number: " + bookingNo);
         }
         if (bookings.contains(bookingNo)) {
             throw new BookHotelFault("Hotel room has already been booked", "Hotel room has already been booked");
@@ -118,7 +123,7 @@ public class HotelDatabase {
             throw new CancelHotelFault("Unable to cancel hotel booking", "Unable to cancel hotel booking");
         }
         if (!bookingNoMap.containsKey(bookingNo)) {
-            throw new CancelHotelFault("No such booking number: "+bookingNo, "No such booking number: "+bookingNo);
+            throw new CancelHotelFault("No such booking number: " + bookingNo, "No such booking number: " + bookingNo);
         }
         if (!bookings.contains(bookingNo)) {
             throw new CancelHotelFault("Hotel room has not been booked", "Hotel room has not been booked");
