09b7d1b4fe976db928897a32c2def42f1b3d176a
Peter Gammelgaard <peter@shapehq.com>
2013-12-01 15:40:25 +0100

    Added some magic draw of rest implementation

--- code/TravelAgencyREST/src/java/ws/dtu/manager/ItineraryDatabase.java
+++ code/TravelAgencyREST/src/java/ws/dtu/manager/ItineraryDatabase.java
@@ -4,6 +4,7 @@
  */
 package ws.dtu.manager;
 import java.util.HashMap;
+import ws.dtu.model.Customer;
 import ws.dtu.model.Itinerary;
 import ws.dtu.model.exceptions;
 
@@ -14,9 +15,10 @@ import ws.dtu.model.exceptions;
 public final class ItineraryDatabase {
     
 
-    private static HashMap<Integer,HashMap<Integer, Itinerary>> data = new HashMap<Integer, HashMap<Integer, Itinerary>>();
+    private static HashMap<Customer,HashMap<Integer, Itinerary>> data = new HashMap<Customer, HashMap<Integer, Itinerary>>();
     
     private static ItineraryDatabase db;
+    private CustomerDatabase customers = CustomerDatabase.getInstance();
     
     public static synchronized ItineraryDatabase getInstance() {
         if(db == null){
@@ -27,39 +29,42 @@ public final class ItineraryDatabase {
     }
     
     public Itinerary get(int customerID, int itinerary_id) {
-        if(!data.containsKey(customerID)) {
+        Customer customer = customers.get(customerID);
+        if(!data.containsKey(customer)) {
             throw new exceptions.NoSuchCustomerException();
         }
-        if(!data.get(customerID).containsKey(itinerary_id)) {
+        if(!data.get(customer).containsKey(itinerary_id)) {
             throw new exceptions.NoSuchItineraryException();
         }
         
-        return data.get(customerID).get(itinerary_id);
+        return data.get(customer).get(itinerary_id);
     }
     
     public void insert (Itinerary itinerary, int customer_id) {
+        Customer customer = customers.get(customer_id);
         HashMap<Integer, Itinerary> map = null;
-        if (data.containsKey(customer_id)) {
-            map = data.get(customer_id);
+        if (data.containsKey(customer)) {
+            map = data.get(customer);
 
         }else {
             map = new HashMap<Integer, Itinerary>();
         }
         
         map.put(itinerary.getID(), itinerary);
-        data.put(new Integer(customer_id), map);
+        data.put(customer, map);
     }
     
     public void delete (int customer_id, int itinerary_id) {
-        if(!data.containsKey(customer_id)) {
+        Customer customer = customers.get(customer_id);
+        if(!data.containsKey(customer)) {
             throw new exceptions.NoSuchCustomerException();
         }
-        if(!data.get(customer_id).containsKey(itinerary_id)) {
+        if(!data.get(customer).containsKey(itinerary_id)) {
             throw new exceptions.NoSuchItineraryException();
         }
         
-        data.get(customer_id).remove(itinerary_id);
-        data.remove(new Integer(itinerary_id));
+        data.get(customer).remove(itinerary_id);
+        data.remove(customer);
     }
     
     public void reset() {
