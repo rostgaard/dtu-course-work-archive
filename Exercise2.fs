e2296783fcfc2b0b7bdf0e46ef24d1fd5e2fcde0
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-10-19 19:58:38 +0200

    Solution for toString method.

--- Exercise2.fs
+++ Exercise2.fs
@@ -11,9 +11,15 @@ type Departments = Node list
     and Node =
         | Department of Name * Income * Departments
 
-let comp1 = Department ("hat",2,[Department("hat",2,[])]);;
 
-let production2 = Department ("production1", 10,[]);
+let production1 = Department ("production1", 10,[]);
+let production2 = Department ("production2", 4,[]);
+let sales = Department ("Sales", -1,[]);
+
+let comp1 = Department ("Top",2,
+                [Department("Middle1",2,
+                     [production1;production2]);
+                 sales]);;
 
 (* Declare a function to extract a list of pairs (department name, gross income), for all departments. *)
 let rec toList = function
@@ -36,11 +42,29 @@ let rec departmentIncome = function
     | Department (name, income, subdeps) -> income +  sumIncome (toList subdeps);;
 
 let rec departmentsIncome = function
-    | Department (name, income, []) -> (name,income)
+    | Department (name, income, []) -> (name,income) //TODO: check if redundant?
     | Department (name, income, rest) -> (name,income +  sumIncome (toList rest));;
 
 (* Declare a function to extract a list of pairs (department name, total income) for all departments. *)
 //TODO
 
 (* Declare a function format of type Department -> string, which can be used to get a textual form of a department such that names of sub-departments will occur suitably indented (e.g., with four spaces) on separate lines. (Use printf to print out the result. Do not use printf in the declaration of format.) *)
-let rec toString 
+
+let rec indention = function
+  | 0 -> ""; 
+  | x -> "    " + indention (x-1);;
+
+let departmentAsString = function
+    | Department (name, income, _) -> name + " (" + string (income) + ")";;
+
+let rec departmentsAsString = function
+    |[] -> ""
+    | Department(name, income, subdeps)::rest -> departmentAsString(Department(name, income, subdeps)) + subDepartments (subdeps, 1) + departmentsAsString (rest)
+and subDepartments = function
+    | [],_         -> ""
+    | Department (name, income, [])::rest  ,level -> "\n" + indention level + departmentAsString (Department (name, income, [])) + subDepartments (rest,level)
+    | Department (name, income, subdeps)::rest,level -> "\n" + indention level + departmentAsString (Department (name, income, [])) + subDepartments (subdeps,level+1) + subDepartments (rest,level) ;;
+
+
+printf "%s\n\n" (departmentsAsString [comp1]);;
+
