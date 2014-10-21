d76d424c3a629645b9f28a64181b814021e45733
Luai Michlawi <s113444@student.dtu.dk>
2014-10-21 07:46:58 +0000

    Assignment4
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e14-groupC@27 a3cd46ed-ea8e-4e4b-8fda-82757e0212fd

---
+++ Tutorials/Assignment4-App/src/com/example/assignment4/WebServiceConnection.java
@@ -0,0 +1,32 @@
+package com.example.assignment4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+public class WebServiceConnection {
+	
+	private static final String BASE_URL = "http://192.168.87.111:8080/Assignment4/rest/events";
+	
+	public static Event invokeAddEventWebServer(int id, float value) {
+		final String url = BASE_URL + "/addEvent?id={id}&value={value}";
+		final Map<String,String> parameters = new HashMap<String,String>();
+		parameters.put("id", String.valueOf(id));
+		parameters.put("value", String.valueOf(value));
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(new MappingJacksonHttpMessageConverter());
+		return restTemplate.getForObject(url, Event.class, parameters);
+	}
+	
+	public static Event invokeAwaitEventWebServer(int id) {
+		final String url = BASE_URL + "/awaitEvent?id={id}";
+		final Map<String,String> parameters = new HashMap<String,String>();
+		parameters.put("id", String.valueOf(id));
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(new MappingJacksonHttpMessageConverter());
+		return restTemplate.getForObject(url, Event.class, parameters);
+	}
+
+}
