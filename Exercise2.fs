1cbc7f027508e86a0e4736d2ba0bc1ea99fedc75
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-10-22 09:45:19 +0200

    Refinements and bugfixes.

--- Exercise2.fs
+++ Exercise2.fs
@@ -12,14 +12,16 @@ type Departments = Node list
         | Department of Name * Income * Departments
 
 
+
 let production1 = Department ("production1", 10,[]);
 let production2 = Department ("production2", 4,[]);
-let sales = Department ("Sales", -1,[]);
+let production3 = Department ("production3", 2,[]);
 
-let comp1 = Department ("Top",2,
-                [Department("Middle1",2,
-                     [production1;production2]);
-                 sales]);;
+let salesShoes = Department ("Sales (Shoes)", -1,[]);;
+let salesHats = Department ("Sales (Hats)", -1,[]);;
+let productionUnits = Department("Production units",2,[production1;production2]);;
+
+let company = Department ("Shoes and hats inc.",-5,[salesHats;salesShoes;productionUnits]);;
 
 (* Declare a function to extract a list of pairs (department name, gross income), for all departments. *)
 let rec toList = function
@@ -29,42 +31,50 @@ and namesElement = function
     | Department (name, income, []) -> [(name,income)]
     | Department (name, income, rest) -> (name,income) :: (toList rest) ;;
     
-toList [comp1];;
+let rec asList = function
+    | Department (name, income, []) -> [(name,income)]
+    | Department (name, income, sub) -> (name,income) :: subdepartments sub
+and subdepartments = function
+    | [] -> []
+    | Department (name, income, sub)::[] -> [(name,income)]
+    | Department (name, income, sub)::rest -> (name,income):: subdepartments (sub) @ subdepartments (rest);;
+ 
+
+let hat = toList [company];;
+let hat1 = asList company;;
 
 let sumIncome departments = List.fold (fun x  (name, income) -> income+x) 0 departments;;
 
-//let sumIncome departments = List.fold (fun x inc -> inc+x) 0 (List.map (fun (name, income) -> income) departments);;
-sumIncome (toList [comp1]);;
+sumIncome (toList [company]);;
 
 (* Declare a function to extract the total income for a given department by adding up its gross income, including the income of its sub-departments. *)
 let rec departmentIncome = function
     | Department (name, income, []) -> income
     | Department (name, income, subdeps) -> income +  sumIncome (toList subdeps);;
 
-let rec departmentsIncome = function
-    | Department (name, income, []) -> (name,income) //TODO: check if redundant?
-    | Department (name, income, rest) -> (name,income +  sumIncome (toList rest));;
-
+departmentIncome (productionUnits);
 (* Declare a function to extract a list of pairs (department name, total income) for all departments. *)
-//TODO
+
+let rec departmentsIncome = function
+    | Department (name, income, []) -> (name,income)
+    | Department (name, income, rest) -> (name,income +  sumIncome (toList rest));;
 
 (* Declare a function format of type Department -> string, which can be used to get a textual form of a department such that names of sub-departments will occur suitably indented (e.g., with four spaces) on separate lines. (Use printf to print out the result. Do not use printf in the declaration of format.) *)
 
-let rec indention = function
-  | 0 -> ""; 
-  | x -> "    " + indention (x-1);;
-
 let departmentAsString = function
-    | Department (name, income, _) -> name + " (" + string (income) + ")";;
+    | Department (name, income, subdeps) -> name + " (" + string (departmentIncome (Department (name, income, subdeps))) + ")";;
 
 let rec departmentsAsString = function
     |[] -> ""
     | Department(name, income, subdeps)::rest -> departmentAsString(Department(name, income, subdeps)) + subDepartments (subdeps, 1) + departmentsAsString (rest)
+and indention = function
+  | 0 -> ""; 
+  | x -> "    " + indention (x-1)
 and subDepartments = function
     | [],_         -> ""
     | Department (name, income, [])::rest  ,level -> "\n" + indention level + departmentAsString (Department (name, income, [])) + subDepartments (rest,level)
     | Department (name, income, subdeps)::rest,level -> "\n" + indention level + departmentAsString (Department (name, income, [])) + subDepartments (subdeps,level+1) + subDepartments (rest,level) ;;
 
 
-printf "%s\n\n" (departmentsAsString [comp1]);;
+printf "%s\n\n" (departmentsAsString [company]);;
 
