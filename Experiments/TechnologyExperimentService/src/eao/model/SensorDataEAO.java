dbec9a7d035c98506e65bc6e218fa0010bdb7372
Luai Michlawi <s113444@student.dtu.dk>
2014-10-22 18:47:16 +0000

    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e14-groupC@29 a3cd46ed-ea8e-4e4b-8fda-82757e0212fd

---
+++ Experiments/TechnologyExperimentService/src/eao/model/SensorDataEAO.java
@@ -0,0 +1,127 @@
+package eao.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import dto.model.Event;
+import entity.model.EventEntity;
+import entity.model.EventEntity.EventType;
+import entity.model.Sensor;
+
+@LocalBean
+@Stateless
+public class SensorDataEAO {
+	
+	@PersistenceContext(unitName = "TechnologyExperimentService")
+	EntityManager em;
+
+	public EventEntity addEvent(float value, int sensorId, EventType eventType) {
+
+		Sensor sensor = em.find(Sensor.class, sensorId);
+		if (sensor == null) {
+			sensor = new Sensor();
+			sensor.setId(sensorId);
+			em.persist(sensor);
+		}
+
+		EventEntity event = new EventEntity();
+		event.setSensor(sensor);
+		event.setValue(value);
+		long time = System.currentTimeMillis();
+		event.setTime(time);
+		event.setEventType(eventType);
+		em.persist(event);
+		em.flush(); // NEEDED for updating the automatically assigned id to the event entity
+		return event;
+	}
+	
+
+	public List<EventEntity> getEventlist(int id) {
+		Sensor sensor = em.find(Sensor.class, id);
+		if (sensor == null) {
+			return Collections.emptyList();
+		}
+		
+		return sensor.getEvents();
+	}
+
+	
+	final private static String querytext =
+		"SELECT e " +
+		"FROM EventEntity e, Sensor s " +
+		"WHERE " + 
+		  "s.id = :id " +
+		"AND " +
+		  "e.sensor = s " +
+		"AND " +
+		  "e.time >  :time " +
+		"ORDER BY e.time ASC";
+	
+	final private static String querytext2 = 
+			"SELECT e " +
+			"FROM EventEntity e, Sensor s " +
+			"WHERE " + 
+			  "s.id = :id " +
+			"AND " +
+			 "e.sensor = s " +
+			"AND " +
+			  "e.eventType =  :eventType ";			
+	
+	// Note: In JPA, you can also define @NamedQueries -- just not here.
+	
+	public List<EventEntity> getEventlist(int id, int time) {		
+		TypedQuery<EventEntity> query = em.createQuery(querytext, EventEntity.class);
+		query.setParameter("id", id);
+		query.setParameter("time", System.currentTimeMillis() - time);
+		List<EventEntity> result = query.getResultList();
+
+		if (result != null) {
+			return result;
+		} else {
+			return Collections.emptyList();
+		}
+	}
+	
+	public List<EventEntity> getEventlist(int id, EventType eventType) {		
+		TypedQuery<EventEntity> query = em.createQuery(querytext2, EventEntity.class);
+		query.setParameter("id", id);
+		query.setParameter("eventType",eventType);
+		List<EventEntity> result = query.getResultList();
+
+		if (result != null) {
+			return result;
+		} else {
+			return Collections.emptyList();
+		}
+	}
+	
+
+	public Event convertEventEntity(EventEntity eventEntity) {
+		int id = eventEntity.getId();
+		float value = eventEntity.getValue();
+		long time = eventEntity.getTime();
+		int sensorId = eventEntity.getSensor().getId();
+		EventType eventType = eventEntity.getEventType();
+		return new Event(id, value, time, sensorId, eventType);
+	}
+	
+	public List<Event> convertEventEntityList(List<EventEntity> list) {
+		List<Event> events = new ArrayList<Event>();
+		
+		for (EventEntity e : list) {
+			events.add(convertEventEntity(e));
+		}
+		
+		return events;
+		
+	}
+
+
+}
---
+++ Experiments/TechnologyExperimentService/src/web/services/EventWebService.java
@@ -0,0 +1,148 @@
+package web.services;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+
+import dto.model.Event;
+import eao.model.SensorDataEAO;
+import entity.model.EventEntity;
+import entity.model.EventEntity.EventType;
+
+@LocalBean
+@Stateless
+@Path("/events")
+public class EventWebService {
+	
+	private static final long TIMEOUT = 9000;
+	private static Map<Integer,List<EventEntity>> entitiesWaiting = new HashMap<Integer,List<EventEntity>>();
+	
+	@EJB SensorDataEAO eao;
+
+	@GET
+	@Path("/addEvent")
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED})
+	@Produces(MediaType.APPLICATION_JSON)
+	public Event addEvent(@QueryParam("value") float value, @QueryParam("id") int id, @QueryParam("eventType") EventType eventType) {	
+		EventEntity eventEntity = eao.addEvent(value, id, eventType);
+		
+		List<EventEntity> tempList = entitiesWaiting.get(id);
+		if (tempList != null) {
+			//tempList = Collections.synchronizedList(tempList);
+			synchronized(tempList) {
+				System.out.println("NOtify");
+				tempList.notifyAll();
+			}
+		}
+		
+		return eao.convertEventEntity(eventEntity);
+	}
+	
+	
+	@GET
+	@Path("/getEvents")
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED})
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Event> getEventList(@QueryParam("id") int id) {
+		List<EventEntity> eventEntities = new ArrayList<EventEntity>();
+		List<Event> events = new ArrayList<Event>();
+		eventEntities = eao.getEventlist(id);
+		events = eao.convertEventEntityList(eventEntities);
+		return events;
+	}
+	
+	@GET
+	@Path("/getEventsByType")
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED})
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Event> getEventList(@QueryParam("id") int id, @QueryParam("eventType") EventType eventType) {
+		List<EventEntity> eventEntities = new ArrayList<EventEntity>();
+		List<Event> events = new ArrayList<Event>();
+		eventEntities = eao.getEventlist(id, eventType);
+		events = eao.convertEventEntityList(eventEntities);
+		return events;
+	}
+	
+	@GET
+	@Path("/getEventsInTimeSpan")
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED})
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Event> getEventList(@QueryParam("id") int id, @QueryParam("time") int time) {
+		List<EventEntity> eventEntities = new ArrayList<EventEntity>();
+		List<Event> events = new ArrayList<Event>();
+		eventEntities = eao.getEventlist(id, time);
+		events = eao.convertEventEntityList(eventEntities);
+		return events;
+	}
+	
+	@GET
+	@Path("/awaitEvent")
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED})
+	@Produces(MediaType.APPLICATION_JSON)
+	public Event awaitEvent(@QueryParam("id") int id, @QueryParam("eventType") EventType eventType) {
+		Event event = null;
+		EventEntity eventEntity = awaitEventEntity(id, eventType);
+		
+		if (eventEntity != null) {
+			event = eao.convertEventEntity(eventEntity);
+		}
+		
+		return event;
+	}
+	
+	private EventEntity awaitEventEntity(int id, EventType eventType) {
+		
+		List<EventEntity> eventEntities = Collections.synchronizedList(eao.getEventlist(id, eventType));
+		
+		synchronized (entitiesWaiting) {
+				entitiesWaiting.put(id, eventEntities);
+		}
+		
+		
+		synchronized (eventEntities) {
+			
+			EventEntity tempEventEntity = null;
+			if (eventEntities.size()>0) {
+				tempEventEntity = eventEntities.get(eventEntities.size()-1);
+			}
+			
+			try {
+				eventEntities.wait(TIMEOUT);
+				
+				eventEntities = eao.getEventlist(id, eventType);
+				
+				if (eventEntities.size()>0) {	
+					EventEntity newEvent = eventEntities.get(eventEntities.size()-1);
+					if (!newEvent.equals(tempEventEntity)) {
+						return newEvent;
+					}
+				}
+				
+				throw new WebApplicationException();
+				//throw new TimeoutException();	
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			entitiesWaiting.remove(id);
+		}
+		
+		return null;
+		
+	}
+}
