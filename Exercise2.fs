47847ff6bf399fde43f4d10001104f065dd85189
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-10-24 00:19:07 +0200

    Simplified the implementation a lot. The toString could also need some work, but will have to wait.

--- Exercise2.fs
+++ Exercise2.fs
@@ -1,9 +1,5 @@
-(* 6.9 A company consists of departments with sub-departments, which again can have sub-departments, and so on. (The company can also be considered as a department.)
-- Assume that each department has a name and a (possibly empty) list of sub-departments. Declare an F# type Department.
-- Extend this type so that each department has its own gross income.
 
-*)
-//TODO add function type declarations
+(* Department type declaration.*)
 type Income = int;;
 type Name   = string;;
 
@@ -11,69 +7,61 @@ type Departments = Node list
     and Node =
         | Department of Name * Income * Departments
 
-
-
-let production1 = Department ("production1", 10,[]);
-let production2 = Department ("production2", 4,[]);
-let production3 = Department ("production3", 2,[]);
+(* Department declarations. *)
+let production1 = Department ("Shoe shrinking", 10,[]);
+let production2 = Department ("Hat feathering ", 4,[]);
+let production3 = Department ("Hat tipping", 2,[]);
 
 let salesShoes = Department ("Sales (Shoes)", -1,[]);;
 let salesHats = Department ("Sales (Hats)", -1,[]);;
-let productionUnits = Department("Production units",2,[production1;production2]);;
+let Marketing = Department ("Marketing", 3, []);;
+let salesManagement = Department ("Sales Management", 2, [salesHats;salesShoes]);;
+let productionUnits = Department("Production units",2,[production1;production2;production3]);;
 
-let company = Department ("Shoes and hats inc.",-5,[salesHats;salesShoes;productionUnits]);;
+let company = Department ("Shoes and hats inc.",-5,[salesManagement;productionUnits]);;
 
-(* Declare a function to extract a list of pairs (department name, gross income), for all departments. *)
+(* Helper function to convert a company tree to a list of companies.*)
 let rec toList = function
+    | Department (name, income, []) -> [Department (name, income, [])]
+    | Department (name, income, sub) -> Department (name, income, sub) :: subDepartments sub
+and subDepartments = function
     | [] -> []
-    | e::es -> (namesElement e) @ (toList es)
-and namesElement = function
-    | Department (name, income, []) -> [(name,income)]
-    | Department (name, income, rest) -> (name,income) :: (toList rest) ;;
-    
-let rec asList = function
-    | Department (name, income, []) -> [(name,income)]
-    | Department (name, income, sub) -> (name,income) :: subdepartments sub
-and subdepartments = function
-    | [] -> []
-    | Department (name, income, sub)::[] -> [(name,income)]
-    | Department (name, income, sub)::rest -> (name,income):: subdepartments (sub) @ subdepartments (rest);;
- 
+    | Department (name, income, sub)::[] -> Department (name, income, sub):: subDepartments (sub)
+    | Department (name, income, sub)::rest -> Department (name, income, sub) :: subDepartments (rest);;
 
-let hat = toList [company];;
-let hat1 = asList company;;
+(* Helper function for summing up the total income of a department list.*)
+let sumIncome departments = List.fold (fun x  (Department (name, income, sub)) -> income+x) 0 departments;;
+let departmentIncome department = sumIncome (toList department);;
 
-let sumIncome departments = List.fold (fun x  (name, income) -> income+x) 0 departments;;
+assert ((departmentIncome productionUnits)= 10+4+2+2);;
 
-sumIncome (toList [company]);;
+(** Implementation **)
 
-(* Declare a function to extract the total income for a given department by adding up its gross income, including the income of its sub-departments. *)
-let rec departmentIncome = function
-    | Department (name, income, []) -> income
-    | Department (name, income, subdeps) -> income +  sumIncome (toList subdeps);;
+(* This method converts a department to a list of tuples containing its sub departments. *)
+let incometuple = function 
+   | Department (name, income, subs) -> (name, income);;
+let departmentsIncome company = List.map (incometuple) (toList company);;
 
-departmentIncome (productionUnits);
-(* Declare a function to extract a list of pairs (department name, total income) for all departments. *)
+(* Total department incomes. *)
+let totalincometuple = function 
+   | Department (name, income, subs) -> (name, departmentIncome (Department (name, income, subs)));;
+let departmentsTotalIncome company = List.map (totalincometuple) (toList company);;
 
-let rec departmentsIncome = function
-    | Department (name, income, []) -> (name,income)
-    | Department (name, income, rest) -> (name,income +  sumIncome (toList rest));;
-
-(* Declare a function format of type Department -> string, which can be used to get a textual form of a department such that names of sub-departments will occur suitably indented (e.g., with four spaces) on separate lines. (Use printf to print out the result. Do not use printf in the declaration of format.) *)
+(* ToString method *)
 
 let departmentAsString = function
-    | Department (name, income, subdeps) -> name + " (" + string (departmentIncome (Department (name, income, subdeps))) + ")";;
+    | Department (name, income, subdeps) -> name + " " + string (income);;
 
 let rec departmentsAsString = function
     |[] -> ""
-    | Department(name, income, subdeps)::rest -> departmentAsString(Department(name, income, subdeps)) + subDepartments (subdeps, 1) + departmentsAsString (rest)
+    | Department(name, income, subdeps)::rest -> departmentAsString(Department(name, income, subdeps)) + subDepartmentString (subdeps, 1) + departmentsAsString (rest)
 and indention = function
   | 0 -> ""; 
   | x -> "    " + indention (x-1)
-and subDepartments = function
+and subDepartmentString = function
     | [],_         -> ""
-    | Department (name, income, [])::rest  ,level -> "\n" + indention level + departmentAsString (Department (name, income, [])) + subDepartments (rest,level)
-    | Department (name, income, subdeps)::rest,level -> "\n" + indention level + departmentAsString (Department (name, income, [])) + subDepartments (subdeps,level+1) + subDepartments (rest,level) ;;
+    | Department (name, income, [])::rest  ,level -> "\n" + indention level + departmentAsString (Department (name, income, [])) + subDepartmentString (rest,level)
+    | Department (name, income, subdeps)::rest,level -> "\n" + indention level + departmentAsString (Department (name, income, [])) + subDepartmentString (subdeps,level+1) + subDepartmentString (rest,level) ;;
 
 
 printf "%s\n\n" (departmentsAsString [company]);;
