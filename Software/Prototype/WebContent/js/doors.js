49762e6eb3c9e2bf033a4fab085f3c6d9b43da7f
Stefan Witold Mertens <s113420@student.dtu.dk>
2014-10-31 14:50:40 +0000

    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e14-groupC@44 a3cd46ed-ea8e-4e4b-8fda-82757e0212fd

--- Software/Prototype/WebContent/js/doors.js
+++
@@ -1,9 +0,0 @@
-  var canvas = document.getElementById('myCanvas');
-      var context = canvas.getContext('2d');
-      var imageObj = new Image();
-		var destX = canvas.width;
-		var destY = canvas.height;
-      imageObj.onload = function() {
-        context.drawImage(imageObj, 0, 0,destX,destY);
-      };
-      imageObj.src = 'floor_plan_example.png';
