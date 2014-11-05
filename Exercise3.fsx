9f9ce7395ae95f27161557a7dae3d94952a0539e
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-11-05 17:44:29 +0100

    Comments and experiments on part 6.

--- Exercise3.fsx
+++ Exercise3.fsx
@@ -4,43 +4,52 @@
  *)
 
 (************
- * Part 1
+ * Part 1 - Type defininition.
  *)
 
  type Prop = 
-          | A  of string
-          | Dis of Prop * Prop
-          | Con of Prop * Prop
-          | Neg of Prop;;
+ | A  of string
+ | Dis of Prop * Prop
+ | Con of Prop * Prop
+ | Neg of Prop;;
 
+(* Return a string representation of a Proposition.
+   Prop -> string *)
 let rec toString = function
  | A p       -> p
  | Dis (p,q) -> "("+toString p + " || " + toString q+")"
  | Con (p,q) -> "("+toString p + " && " + toString q+")"
  | Neg p     -> "!"+toString p
 
+(* Return a string representation of a literal set.
+   Set<Prop> -> string *)
+let lsToString = function 
+  |  []    -> "()"
+  |  x::[] -> toString x
+  |  x::xs -> List.fold (fun s prop -> s + " && " +  toString (prop)) (toString x) xs;;
+
 (************
  * Part 2
  *)
-let rec deMorgan = function 
+let rec negNormalForm = function 
  | A p         -> A p
- | Dis (p,q)   -> Neg (Con (Neg (deMorgan p), Neg (deMorgan q)))
- | Con (p,q)   -> Neg (Dis (Neg (deMorgan p), Neg (deMorgan q)))
- | Neg (Neg p) -> deMorgan p
- | Neg p       -> Neg (deMorgan p);;
+ | Dis (p,q)   -> Neg (Con (Neg (negNormalForm p), Neg (negNormalForm q)))
+ | Con (p,q)   -> Neg (Dis (Neg (negNormalForm p), Neg (negNormalForm q)))
+ | Neg (Neg p) -> negNormalForm p
+ | Neg p       -> Neg (negNormalForm p);;
 
 let prop1 = Dis (A "p", A "q");;
 let exptProp1 = Con (Neg (A "p"),Neg (A "q"));;
-let resProp1 = deMorgan prop1 ;;
+let resProp1 = negNormalForm prop1 ;;
 
 let prop2 = Con (A "p", A "q");;
 let exptProp2 = Dis (Neg (A "p"),Neg (A "q"));;
-let resProp2 = deMorgan prop2 ;;
+let resProp2 = negNormalForm prop2 ;;
  
 let prop3 = A "p";;
-let resProp3 = deMorgan prop3 ;;
+let resProp3 = negNormalForm prop3 ;;
 
-let resProp4 = deMorgan (Neg prop2) ;;
+let resProp4 = negNormalForm (Neg prop2) ;;
 
 (************
  * Part 3
@@ -82,8 +91,6 @@ let rec dnfToSetAux a ls =
 
 dnfToSetAux (normalForm prop4) Set.empty
 
-
-
 //toString (complement (Neg (Neg(A "a"))));;
 let testSet = litOf (Con (A "a", Neg (A "b")));;
 
@@ -106,22 +113,43 @@ let complement = function
  *  intersected values to verify consistency.
  *)
 
-let isConsistent ls = Set.count (Set.intersect (Set.map (fun (p) -> complement (p)) ls) ls) = 0; 
+let inconsistenValues ls = Set.intersect (Set.map (fun (p) -> complement (p)) ls) ls;;
+let isConsistent ls = Set.isEmpty (inconsistenValues ls); 
 
-let testConsistent = (isConsistent testSet = true);;
+let testConsistentSet = litOf (Con (A "a", Neg (A "b")));;
+let testInconsistentSet = litOf (Con (A "a", Neg (A "a")));;
 
+assert (isConsistent testConsistentSet = true);;
+assert (isConsistent testInconsistentSet = false);;
+
+(* Removes inconsistent litterals from the set by using the set difference operator.*)
+let removeInconsistent ls = Set.difference (inconsistenValues ls) ls;;
+
+assert (not (Set.isEmpty (removeInconsistent testConsistentSet)));;
+assert (Set.isEmpty (removeInconsistent testInconsistentSet));;
+
+
+
+lsToString (Set.toList testConsistentSet);;
+
+ (*//Declare an F# function toDNFsets that transforms an arbitrary proposition into a dns set with just consistent literal sets.*)
+
+(* Prop -> Set<Set<Prop>> *)
+let rec toDNFsetsAux prop set = function
+ | A p                 -> Set.add (litOf (A p)) set
+ | Con (Con (p,q), r)  -> Set.union (toDNFsetsAux (Con (p,q)) set) (Set.add (litOf (Con (p,q))) set)
+ | Con (p, Con (q, r)) -> Set.add (litOf (Con (p,q))) set
+ | Con (p,q) -> Set.add (litOf (Con (p,q))) set
+ | _         -> set;;
+
+ toDNFsetsAux testConsistentSet Set.empty;;
 
 (************
  * Part 6
  *)
- (*Declare an F# function
-toDNFsets
-that transforms an arbitrary proposition into a dns set
-with just consistent literal sets.*)
- let toDNFsets = None;;
 
-(*Declare a function impl a b for computing the implication a => b
-and a function iff a b for computing the bi-implication a <=> b*)
- let impl = None;
- let iff = None;
+(*//Declare a function impl a b for computing the implication a => b
+  and a function iff a b for computing the bi-implication a <=> b*)
+ let impl = None;;
+ let iff = None;;
 
