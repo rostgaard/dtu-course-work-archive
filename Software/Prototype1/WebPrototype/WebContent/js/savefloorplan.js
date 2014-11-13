59402f28b8cfd80998b499218cca5ffed91e470c
Jesper Vestergaard Mark <s124259@student.dtu.dk>
2014-11-13 23:29:23 +0000

    typo
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e14-groupC@110 a3cd46ed-ea8e-4e4b-8fda-82757e0212fd

--- Software/Prototype1/WebPrototype/WebContent/js/savefloorplan.js
+++ Software/Prototype1/WebPrototype/WebContent/js/savefloorplan.js
@@ -2,5 +2,5 @@ function saveFloorplan(){
 	var offsets = document.getElementById('sensor').getBoundingClientRect();
 	var top = offsets.top;
 	var left = offsets.left;
-	alert("Coordinates for Sensor 1: x = " + top + ", y = " + left);
+	alert("Coordinates for Sensor 1: x = " + left + ", y = " + top);
 }
