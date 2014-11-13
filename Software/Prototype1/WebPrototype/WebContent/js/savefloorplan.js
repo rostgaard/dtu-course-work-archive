f40d425dae3b0cb9525bba2278cb499029fde3f9
Jesper Vestergaard Mark <s124259@student.dtu.dk>
2014-11-13 23:24:12 +0000

    Drag and drop sensors to floor plan
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e14-groupC@109 a3cd46ed-ea8e-4e4b-8fda-82757e0212fd

---
+++ Software/Prototype1/WebPrototype/WebContent/js/savefloorplan.js
@@ -0,0 +1,6 @@
+function saveFloorplan(){
+	var offsets = document.getElementById('sensor').getBoundingClientRect();
+	var top = offsets.top;
+	var left = offsets.left;
+	alert("Coordinates for Sensor 1: x = " + top + ", y = " + left);
+}
