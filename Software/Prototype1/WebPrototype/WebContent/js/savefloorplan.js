0709cff6ef5ca9597cd61e8216a550a9505ba043
Jesper Vestergaard Mark <s124259@student.dtu.dk>
2014-11-14 12:36:01 +0000

    minor refinement in js save floorplan
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e14-groupC@113 a3cd46ed-ea8e-4e4b-8fda-82757e0212fd

--- Software/Prototype1/WebPrototype/WebContent/js/savefloorplan.js
+++ Software/Prototype1/WebPrototype/WebContent/js/savefloorplan.js
@@ -1,6 +1,6 @@
 function saveFloorplan(){
 	var offsets = document.getElementById('sensor').getBoundingClientRect();
-	var top = offsets.top;
-	var left = offsets.left;
-	alert("Coordinates for Sensor 1: x = " + left + ", y = " + top);
+	var sensor1_y = offsets.top;
+	var sensor1_x = offsets.left;
+	alert("Coordinates for Sensor 1: x = " + sensor1_x + ", y = " + sensor1_y);
 }
