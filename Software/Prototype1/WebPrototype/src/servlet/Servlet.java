1f0641a5952641b16bf17b90a6d03cc07e838a68
Jesper Vestergaard Mark <s124259@student.dtu.dk>
2014-11-18 18:00:30 +0000

    Servlet for login
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e14-groupC@135 a3cd46ed-ea8e-4e4b-8fda-82757e0212fd

---
+++ Software/Prototype1/WebPrototype/src/servlet/Servlet.java
@@ -0,0 +1,60 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dto.model.User;
+import eao.model.Conversion;
+import eao.model.UserEAO;
+import entity.model.UserEntity;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	UserEAO eao;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		if(request.getParameter("login") != null){
+			String userName = request.getParameter("username");
+			String password = request.getParameter("password");
+
+			System.out.println(userName + " " + password);
+			try{
+				UserEntity userEntity = eao.getUserByUserName(userName);
+				User user = Conversion.convertUserEntity(userEntity);
+				if(password.equals(user.getPassword())){
+
+					HttpSession session = request.getSession();
+					session.setAttribute("user", user);
+					response.sendRedirect("dashboard.jsp");
+				}
+				else{
+					response.sendRedirect("default.jsp");
+				}
+
+			}
+			catch(IllegalArgumentException | NullPointerException e){
+				response.sendRedirect("default.jsp");
+			}
+		}
+	}
+}
