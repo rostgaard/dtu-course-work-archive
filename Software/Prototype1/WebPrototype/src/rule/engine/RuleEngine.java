a82046d8adfb687472b4bca9e72566e593197065
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-11-19 08:19:42 +0000

    Comments and minor change to RuleEngine interface.
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e14-groupC@136 a3cd46ed-ea8e-4e4b-8fda-82757e0212fd

--- Software/Prototype1/WebPrototype/src/rule/engine/RuleEngine.java
+++ Software/Prototype1/WebPrototype/src/rule/engine/RuleEngine.java
@@ -3,9 +3,11 @@ package rule.engine;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import dto.model.Event;
@@ -13,12 +15,21 @@ import dto.model.RuleString;
 
 public class RuleEngine {
 
-	final Map<Integer,List<Rule>> policy = new HashMap<>();
-	
-	private static final Logger log = Logger.getLogger(RuleEngine.class.getName());
-	
+	// FIXME: The current global security level of the RuleEngine.
 	public static int systemSecurityLevel = 1;
 	
+	// Internal logger.
+	private static final Logger log = Logger.getLogger(RuleEngine.class.getName());
+
+	// Rule storage.
+	final Map<Integer,List<Rule>> policy = new HashMap<>();
+	
+	/**
+	 * Constructs a new RuleEngine object. Used in conjunction with
+	 * {@code parseRule} method (or the plural equivalent).
+	 * 
+	 * @param A list of Rule objects to add to this engine.
+	 */
 	public RuleEngine (List<Rule> rules) {
 		for (Rule rule : rules) {
 			if (this.policy.containsKey(rule.getPolicyID())) {
@@ -32,11 +43,13 @@ public class RuleEngine {
 	}
 	
 	/**
+	 * Converts a RuleString DTO object to a list of rules.
 	 * 
-	 * @param ruleString
-	 * @return
+	 * @param ruleString The RuleString DTO object to convert
+	 * @return A list of Rule objects
 	 */
 	public static List<Rule> parseRule (RuleString ruleString) {
+		// We skip over ruleStrings that are disabled.
 		if (!ruleString.isStatus()) {
 			return new LinkedList<Rule>();
 		}
@@ -52,6 +65,15 @@ public class RuleEngine {
 		return rules;
 	}
 	
+	/**
+	 * Convenience method for converting a list of RulesString DTO's 
+	 * rather than just one. The method merges Rule objects generated 
+	 * from each list into a single list. The reversal of this operation
+	 * is possible by using the {@code ruleStrings} method.
+	 * 
+	 * @param ruleStrings The list of RuleString DTO objects to convert
+	 * @return A list of Rule objects
+	 */
 	public static List<Rule> parseRules (List<RuleString> ruleStrings) {
 		List<Rule> rules = new LinkedList<Rule>();
 		
@@ -61,7 +83,13 @@ public class RuleEngine {
 		
 		return rules;
 	}
-
+	
+	/**
+	 * Extracts the Rule objects currently associated with this engine
+	 * as a list of RuleString DTO objects.
+	 * 
+	 * @return
+	 */
 	public List<RuleString> ruleStrings () {
 		List<RuleString> ruleStrings = new LinkedList<RuleString>();
 		
@@ -80,9 +108,10 @@ public class RuleEngine {
 	}
 
 	/**
+	 * Serialize a list of Rule objects to a (parseable) string format.
 	 * 
-	 * @param rules
-	 * @return
+	 * @param rules The list of rules to serialize.
+	 * @return A parseable string representation of the rules 
 	 */
 	public static String serializeRules (List<Rule> rules) {
 		String buffer = "";
@@ -95,14 +124,20 @@ public class RuleEngine {
 	}
 	
 	/**
+	 * Checks an event against the rules currently active in the 
+	 * RuleEngine and returns a set of rules that responds to this
+	 * event. It is similar in semantics to a filter operation, where
+	 * the rules are regarded as filter.
+	 * 
 	 * TODO: Check for duplicates. Either by storing in a set or performing a contains operation.
-	 * @param event
-	 * @return
+	 * 
+	 * @param event The event to check
+	 * @return A set of rules that matches.
 	 */
-	public List<Rule> checkEvent (Event event) {
+	public Set<Rule> checkEvent (Event event) {
 		log.finest("Checking event " + event);
 		
-		List<Rule> matches = new LinkedList<Rule>(); 
+		Set<Rule> matches = new HashSet<Rule>(); 
 		
 		for (List<Rule> rules : this.policy.values()) {
 			for (Rule rule : rules) {
