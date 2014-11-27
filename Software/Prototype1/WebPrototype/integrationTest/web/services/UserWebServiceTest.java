1c80216390b3a902abc65bc1eba65137ebb3b768
Jesper Vestergaard Mark <s124259@student.dtu.dk>
2014-11-27 14:51:35 +0000

    Setting up test suite
    git-svn-id: https://svn.imm.dtu.dk/se2/svn/e14-groupC@217 a3cd46ed-ea8e-4e4b-8fda-82757e0212fd

---
+++ Software/Prototype1/WebPrototype/integrationTest/web/services/UserWebServiceTest.java
@@ -0,0 +1,49 @@
+package web.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class UserWebServiceTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserByUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLastLoginByUserName() {
+		fail("Not yet implemented");
+	}
+
+}
