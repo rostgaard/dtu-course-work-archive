2819c6423d87ef8425d53b7e6f6bf62948e5b475
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-01-16 14:27:27 +0100

    Moved the event queue to a seperate file so other scripts may reuse it.

---
+++ Project3/AsyncEventQueue.fs
@@ -0,0 +1,23 @@
+module AsyncEventQueue
+
+// An asynchronous event queue kindly provided by Don Syme 
+type AsyncEventQueue<'T>() = 
+    let mutable cont = None 
+    let queue = System.Collections.Generic.Queue<'T>()
+    let tryTrigger() = 
+        match queue.Count, cont with 
+        | _, None -> ()
+        | 0, _ -> ()
+        | _, Some d -> 
+            cont <- None
+            d (queue.Dequeue())
+
+    let tryListen(d) = 
+        if cont.IsSome then invalidOp "multicast not allowed"
+        cont <- Some d
+        tryTrigger()
+
+    member x.Post msg = queue.Enqueue msg; tryTrigger()
+    member x.Receive() = 
+        Async.FromContinuations (fun (cont,econt,ccont) -> 
+            tryListen cont)
