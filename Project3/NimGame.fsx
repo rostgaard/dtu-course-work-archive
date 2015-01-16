343d0869730c0fefb1fa301c706fb10269415624
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-01-16 16:12:48 +0100

    Clicky-remove-matches-stuff for game implemented. There is a bug in some array offset unfortunately :-(

--- Project3/NimGame.fsx
+++ Project3/NimGame.fsx
@@ -1,6 +1,9 @@
 #load "AsyncEventQueue.fs"
+#load "NimGameUIComponents.fs"
+
 
 // Prelude
+open NimGameUIComponents
 open AsyncEventQueue
 open System 
 open System.Net 
@@ -11,7 +14,20 @@ open System.Drawing
 type NimPlayer = AI | Human;;
 
 type NimGameState = State of List<int>
-type GameEvent = EndTurn of NimPlayer | Move of int * int | EndGame | StartGame
+type GameEvent = EndTurn of NimPlayer | Move of NimGameState| EndGame | StartGame
+
+let gameStateToString (gs : List<int>)  = string (List.map string gs);; 
+
+let reflectMove (state:NimGameState) 
+                (row:int)
+                (col:int) = printf "%s" (string state)
+                            match state with
+                            | State []    -> failwith "Bad state!"
+                            | State s -> let listAsArr   = Array.ofList s
+                                         let heapSize    = Array.get listAsArr row-1 //Account for index skew.
+                                         let newHeapSize = heapSize - (heapSize - (col-1))
+                                         Array.set listAsArr (row-1) newHeapSize
+                                         State (List.ofArray listAsArr)
 
 (* 
    Returns a gamestate from a string buffer. The buffer must be formatted so that
@@ -35,12 +51,84 @@ type NimGame =
                        | Nim (state,AI)    -> gameEvent.Post (EndTurn AI)
                                               Nim (state,Human)
   member obj.move row column = match obj with
-                               | Nim (state,player) -> gameEvent.Post (Move (row,column)) 
-                                                       Nim (state,AI)
+                               | Nim (state,player) -> let newState = reflectMove state row column
+                                                       gameEvent.Post (Move (newState)) 
+                                                       Nim (newState,AI)
   static member create intialState = Nim (State intialState, Human)
 
 
-let mutable ng = NimGame.create ([2;3]);;
+//ng <- ng.endTurn;;
+///ng <- ng.move 1 2;;
+//ng <- ng.endTurn;;
+
+let numberOfHeaps = 5;;
+let matches = [2;3;4;5;6];;
+
+// The window part
+let maxMatches matches = List.max matches;;
+let matchW = 50;;
+let matchH = 80;;
+let totalMatchW = ((maxMatches matches) + 1) * matchW;;
+let totalMatchH = (numberOfHeaps + 1) * matchH;;
+let buttonW = 200;;
+let buttonH = 400;;
+let matchIcon = Image.FromFile("Match_Icon_small.png");;
+
+let matchPanel  = new Panel(Location = Point(0,0), Size = Size(totalMatchW + buttonW, (max totalMatchH buttonH)), BackColor = Color.Black);;
+let buttonPanel = new Panel(Location = Point(0,matchPanel.Height), Size = Size(buttonW, 300), BackColor = Color.White);;
+
+let window =
+  new Form(Text="Nim game", Size= Size(max matchPanel.Width buttonPanel.Width, 
+                                       matchPanel.Height + buttonPanel.Height), 
+                                       AutoScroll = true);;
+
+  let matchButton (x : int) (y : int) z onClick = 
+    let btn = new Button(Location = Point(x,y), MinimumSize=Size(25,matchH),
+                         MaximumSize=Size(20,100),Text= z, BackColor = Color.Black, Image = matchIcon, FlatStyle = FlatStyle.Flat)
+    btn.Click.Add (onClick)
+    btn
+ 
+let startButton =
+  new Button(Location=Point(30,30),MinimumSize=Size(100,50),
+              MaximumSize=Size(100,50),Text="Start new game")
+
+let clearButton =
+  new Button(Location=Point(matchPanel.Height,165),MinimumSize=Size(100,50),
+              MaximumSize=Size(100,50),Text="End move")
+
+let cancelButton =
+  new Button(Location=Point(matchPanel.Height,265),MinimumSize=Size(100,50),
+              MaximumSize=Size(100,50),Text="Give up")
+
+              
+let ansBox =
+  new TextBox(Location=Point(totalMatchW,320),Size=Size(100,25))
+
+  
+let urlBox =
+  new TextBox(Location=Point(totalMatchW,10),Size=Size(400,25))
+
+
+let mutable ng = NimGame.create (matches);;
+
+let handleMove (row:int, column:int) = //printf "%d %d \n" row column
+                                       ng <- (ng.move row column)
+
+// Initialization
+let rec generateMatches level = function
+    | []    -> []
+    | x::xs -> generateHeap level x @ generateMatches (level+1) xs
+and generateHeap level = function
+  | 0   -> []
+  | n   -> ((upcast (matchButton (n*matchW-matchW/2) (level*matchH-matchH/2) (string n) (fun (_) -> handleMove (level,n)) )) : Control)::(generateHeap level (n-1));;
+
+//let disable bs = 
+//    for b in [startButton;clearButton;cancelButton] do 
+//        b.Enabled  <- true
+//     for (b:Button) in bs do 
+//        b.Enabled  <- false
+
+
 
 let rec ready () =
     async {
@@ -48,7 +136,7 @@ let rec ready () =
        let! msg = gameEvent.Receive()
        match msg with 
        | EndGame        -> return! finish()
-       | Move (x,y)     -> return! move(x,y)
+       | Move (state)   -> return! move(state)
        | EndTurn player -> return! nextPlayer(player)
        | StartGame      -> return! setupBoard()
     }
@@ -59,18 +147,23 @@ and setupBoard() =
         let! msg = gameEvent.Receive()
         match msg with 
          | EndGame        -> return! finish()
-         | Move (x,y)     -> return! move(x,y)
+         | Move (state)   -> return! move(state)
          | EndTurn player -> return! nextPlayer(player)
          | StartGame      -> return! setupBoard()
         }
 
-and move (x,y) =
+and move (state) =
+    matchPanel.Controls.Clear ()    
+    match state with
+      | State [] -> failwith "Bad state"
+      | State s  -> matchPanel.Controls.AddRange (List.toArray (generateMatches 1 s))
+
     async {
         printf "Move: Got event!\n"
         let! msg = gameEvent.Receive()
         match msg with 
          | EndGame        -> return! finish()
-         | Move (x,y)     -> return! move(x,y)
+         | Move (state)   -> return! move(state)
          | EndTurn player -> return! nextPlayer(player)
          | StartGame      -> return! setupBoard()
         }
@@ -81,7 +174,7 @@ and finish () =
         let! msg = gameEvent.Receive()
         match msg with 
          | EndGame        -> return! finish()
-         | Move (x,y)     -> return! move(x,y)
+         | Move (state)   -> return! move(state)
          | EndTurn player -> return! nextPlayer(player)
          | StartGame      -> return! setupBoard()
         }
@@ -92,13 +185,24 @@ and nextPlayer (player) =
         let! msg = gameEvent.Receive()
         match msg with 
          | EndGame        -> return! finish()
-         | Move (x,y)     -> return! move(x,y)
+         | Move (state)   -> return! move(state)
          | EndTurn player -> return! nextPlayer(player)
          | StartGame      -> return! setupBoard()
         }
+
+buttonPanel.Controls.Add startButton
+buttonPanel.Controls.Add urlBox
+buttonPanel.Controls.Add ansBox
+buttonPanel.Controls.Add clearButton
+buttonPanel.Controls.Add cancelButton
+matchPanel.Controls.AddRange (List.toArray (generateMatches 1 matches));;
+window.Controls.Add matchPanel
+window.Controls.Add buttonPanel
+
+startButton.Click.Add (fun _ -> gameEvent.Post (StartGame))
+//clearButton.Click.Add (fun _ -> ev.Post Clear)
+//cancelButton.Click.Add (fun _ -> ev.Post Cancel)
 Async.StartImmediate (ready());
 
-ng <- ng.endTurn;;
-ng <- ng.move 1 2;;
-ng <- ng.endTurn;;
+Application.Run(window)
 
