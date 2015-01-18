74a3e337a9c9149ab2b9810f21aad98cc9a66e35
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-01-18 21:31:54 +0100

    Example script for running the launcher.

---
+++ Project3/NimMain.fsx
@@ -0,0 +1,18 @@
+﻿
+
+#r "AsyncEventQueue.dll"
+#r "NimLauncher.dll"
+
+// Prelude
+open NimLauncher
+open System 
+open System.Net 
+open System.Threading 
+open System.Windows.Forms 
+open System.Drawing 
+
+let baseUrl = "http://ada-dk.org/files/";;
+let games = ["1.nimgame"; "2.nimgame"; "nonexisting.nimgame"];;
+
+let launcher = NimLauncher.create (List.map (fun (item) -> baseUrl + item) games) ;;
+Application.Run (NimLauncher.window launcher);;
