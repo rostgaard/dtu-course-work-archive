a5463aab6f4388247cd051bff3dc15aa0e61af00
Anna Maria Walach <jahi.wh+git@gmail.com>
2015-01-19 13:00:18 +0100

    Changes to "Download & Launch" game graphical interface.

--- Project3/NimLauncher.fs
+++ Project3/NimLauncher.fs
@@ -6,6 +6,10 @@ open System.Threading
 open System.Windows.Forms 
 open System.Drawing 
 
+
+
+System.IO.Directory.SetCurrentDirectory __SOURCE_DIRECTORY__;;
+
 type AsyncEventQueue<'T>() = 
     let mutable cont = None 
     let queue = System.Collections.Generic.Queue<'T>()
@@ -29,7 +33,7 @@ type AsyncEventQueue<'T>() =
 
 // An enumeration of the possible events 
 type Message =
-  | Download of string * Button| Cancel | Web of string | Error | Cancelled 
+  | Download of string * Control * Control| Cancel | Web of string | Error | Cancelled 
 
 // Global event queue.
 let ev = AsyncEventQueue<Message>();;
@@ -38,73 +42,60 @@ type NimLauncher = L of List<string> * AsyncEventQueue<Message>;;
 
 let startGame stringBuffer = printf "FIXME: Starting game %s\n" stringBuffer
 
+let winX = 640;;
+let winY = 800;;
 // The window part
-let maxMatches matches = List.max matches;;
 let matchW = 50;;
 let matchH = 80;;
-let totalMatchW = (5 + 1) * matchW;;
-let totalMatchH = (5 + 1) * matchH;;
-let buttonW = 200;;
-let buttonH = 400;;
-let matchIcon = Image.FromFile("Match_Icon_small.png");;
+
+let margin = 100;;
 
 let form =
-  new Form(Text="Nim game launcher", Size=Size(400,600))
+  new Form(Text="Nim game launcher", Size=Size(winX,winY), AutoScroll = true)
 
+
+let urlBox =
+  new TextBox(Location=Point(30,30),Size=Size(200,25));;
+
+let downloadButton = 
+  new Button(Location=Point(30, 70),MinimumSize=Size(60,35),
+              MaximumSize=Size(90,35),Text="Download your game");;
+
+let launchButton =
+  new Button(Location=Point(110,70),MinimumSize=Size(60,35),
+              MaximumSize=Size(90,35),Text="Launch your game");;
 let status  =
-  new Label (Location=Point(30,30),Size=Size(200,30))
+  new Label (Location=Point(30,margin+30),Size=Size(200,30))
 
-let button1 =
-  new Button(Location=Point(30,60),MinimumSize=Size(60,30),
-              MaximumSize=Size(90,30),Text="Download Game 1")
+let makeDownloadButton x y text = 
+  new Button(Location=Point(x,y),MinimumSize=Size(60,35),
+              MaximumSize=Size(90,35),Text=text)
 
-let button1launch =
-  new Button(Location=Point(120,60),MinimumSize=Size(60,30),
-              MaximumSize=Size(90,30),Text="Launch")
-
-let button2 =
-  new Button(Location=Point(30,90),MinimumSize=Size(60,30),
-              MaximumSize=Size(90,30),Text="Download Game 2")
-
-let button2launch =
-  new Button(Location=Point(120,90),MinimumSize=Size(60,30),
-              MaximumSize=Size(90,30),Text="Launch")
-
-let button3 =
-  new Button(Location=Point(30,120),MinimumSize=Size(60,30),
-              MaximumSize=Size(90,30),Text="Download Game 3")
-
-let button3launch =
-  new Button(Location=Point(120,120),MinimumSize=Size(60,30),
-              MaximumSize=Size(90,30),Text="Launch")
+let makeLaunchButton x y = 
+  new Button(Location=Point(x,y),MinimumSize=Size(60,35),
+              MaximumSize=Size(90,35),Text="Launch");;
 
 let cancelButton =
-  new Button(Location=Point(30,150),MinimumSize=Size(60,30),
+  new Button(Location=Point(250,margin+30),MinimumSize=Size(60,30),
               MaximumSize=Size(90,30),Text="Abort download")
 
 let disable bs = 
-    for b in [button1;button2;button3] do 
-        b.Enabled  <- true
-    for (b:Button) in bs do 
+    for (b : Control) in bs do 
         b.Enabled  <- false
-
-let downloadButton (x : int) (y : int) z = 
-  new Button(Location = Point(x,y), MinimumSize=Size(25,matchH),
-              MaximumSize=Size(20,100),Text= z, BackColor = Color.Black, Image = matchIcon, FlatStyle = FlatStyle.Flat)
 
 // StateMachine
 let rec ready() = 
   async {status.Text <- "Ready to download a game."
          let! msg = ev.Receive()
          match msg with
-         | Download (url,launchButton) -> return! loading(url, launchButton)
+         | Download (url,launchButton, cancelButton) -> return! loading(url, launchButton, cancelButton)
          | Cancel       -> return! ready()
          | _            -> failwith("ready: unexpected message")}
   
-and loading(url, launchButton) =
+and loading(url, launchButton, cancelButton) =
   async {status.Text <- "Downloading"
          use ts = new CancellationTokenSource()
-
+         cancelButton.Enabled <- true
           // start the load
          Async.StartWithContinuations
              (async { let webCl = new WebClient()
@@ -114,8 +105,8 @@ and loading(url, launchButton) =
               (fun _ -> ev.Post Error),
               (fun _ -> ev.Post Cancelled),
               ts.Token)
-         disable [button1; button2; button3]   
          let! msg = ev.Receive()
+         cancelButton.Enabled <- false
          match msg with
          | Web html ->
              let _ = html
@@ -131,8 +122,6 @@ and loading(url, launchButton) =
 
 and cancelling() =
   async {status.Text <- "Cancelling"
-         
-         disable [button1; button2; button3]   
          let! msg = ev.Receive()
          match msg with
          | Cancelled | Error | Web  _ ->
@@ -140,37 +129,45 @@ and cancelling() =
          | _    ->  failwith("cancelling: unexpected message")}
 
 and finished(s) =
-  async {status.Text <- s
+  async {status.Text <-  s.Replace("\n", " ")
          disable [cancelButton]
          let! msg = ev.Receive()
          match msg with
          | Cancel -> return! ready()
-         | Download (url, launchButton) -> return! loading(url, launchButton)
+         | Download (url, launchButton, cancelButton) -> return! loading(url, launchButton, cancelButton)
          | _      ->  failwith("finished: unexpected message")}
 
+let rec generateBtns n = function 
+ | []   -> []
+ | x::xs -> let dwBtn = (upcast (makeDownloadButton 30 ((n+1)*40+margin+25)  ("Download game" + string (n+1))) : Control)
+            let lncBtn = (upcast (makeLaunchButton 115 ((n+1)*40+margin+25)) : Control)
+            (dwBtn, lncBtn, x) :: generateBtns (n+1) xs
 
-
+let rec generateClickOptions cancelButton = function
+    | []    -> ()
+    | (dwBtn : Control, lncBtn, x) :: xs -> dwBtn.Click.Add (fun (_) -> printf "%s" x
+                                                                        ev.Post (Download (x, lncBtn, cancelButton)))
+                                            generateClickOptions cancelButton xs;;
+let bgImage = Image.FromFile("kitty_cover.jpg");;
 let create urls = L (urls, ev);;
 //let show   : NimLancher   -> NimLancher
 let rec window l =
     match l with 
-    | L (urlList, ev) -> let panel =  new Panel (Location = Point(0,0), Size = Size(200, 300))
-                         //let buttons = List.fo map (fun url -> url) urlList
-                         disable [cancelButton; button1launch; button2launch; button3launch]
-
+    | L (urlList, ev) -> let panel =  new Panel (Location = Point(0,0), Size = Size(winX, winY), BackgroundImage = bgImage)
+                         let buttons = generateBtns 0 urlList
+                         let (dwBtns : Control list, lncBtns, y) = List.unzip3 buttons
+                         generateClickOptions cancelButton buttons
+                         disable ([cancelButton] @ lncBtns)
+                         downloadButton.Click.Add (fun (_) -> ev.Post (Download (urlBox.Text, launchButton, cancelButton)))
                          panel.Controls.Add status
+                         //cal a function with urlList argument that generates buttons and applies click function and generates 
 
-                         button1.Click.Add (fun (_) -> ev.Post (Download ((Array.get (Array.ofList urlList) 0), button1launch)))
-                         button2.Click.Add (fun (_) -> ev.Post (Download ((Array.get (Array.ofList urlList) 1), button2launch)))
-                         button3.Click.Add (fun (_) -> ev.Post (Download ((Array.get (Array.ofList urlList) 2), button3launch)))
-
-                         panel.Controls.Add button1
-                         panel.Controls.Add button2
-                         panel.Controls.Add button3
+                         panel.Controls.AddRange (List.toArray dwBtns)
+                         panel.Controls.AddRange (List.toArray lncBtns)
                          panel.Controls.Add cancelButton
-                         panel.Controls.Add button1launch
-                         panel.Controls.Add button2launch
-                         panel.Controls.Add button3launch
+                         panel.Controls.Add urlBox
+                         panel.Controls.Add downloadButton
+                         panel.Controls.Add launchButton
                          form.Controls.Add panel
                          Async.StartImmediate (ready())
                          form
--- Project3/NimMain.fsx
+++ Project3/NimMain.fsx
@@ -1,6 +1,4 @@
 ﻿
-
-#r "AsyncEventQueue.dll"
 #r "NimLauncher.dll"
 
 // Prelude
