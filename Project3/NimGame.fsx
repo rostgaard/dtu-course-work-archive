39683ff4da16566ec0c83bc6d97c535f09d22fff
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-01-19 13:08:01 +0100

    Updated state machine to forward state.

--- Project3/NimGame.fsx
+++ Project3/NimGame.fsx
@@ -1,5 +1,3 @@
-#load "AsyncEventQueue.fs"
-open AsyncEventQueue
 open System 
 open System.Net 
 open System.Threading 
@@ -9,11 +7,32 @@ open System.Drawing
 
 System.IO.Directory.SetCurrentDirectory __SOURCE_DIRECTORY__;;
 
+type AsyncEventQueue<'T>() = 
+    let mutable cont = None 
+    let queue = System.Collections.Generic.Queue<'T>()
+    let tryTrigger() = 
+        match queue.Count, cont with 
+        | _, None -> ()
+        | 0, _ -> ()
+        | _, Some d -> 
+            cont <- None
+            d (queue.Dequeue())
+
+    let tryListen(d) = 
+        if cont.IsSome then invalidOp "multicast not allowed"
+        cont <- Some d
+        tryTrigger()
+
+    member x.Post msg = queue.Enqueue msg; tryTrigger()
+    member x.Receive() = 
+        Async.FromContinuations (fun (cont,econt,ccont) -> 
+            tryListen cont)
+
 
 type NimPlayer = AI | Human;;
 
 type NimGameState = State of List<int>
-type GameEvent = EndTurn of NimGameState * NimPlayer | Move of NimGameState| EndGame | StartGame
+type GameEvent = Move of int * int | EndGame | StartGame of NimGameState
 
 let gameStateToString (gs : List<int>)  = string (List.map string gs);; 
 
@@ -21,8 +40,9 @@ let rec calculateM n = function
     | []    -> n
     | x::xs -> calculateM (n ^^^ x) xs;;
 
+
 let rec getNewAk m index = function 
- | []    -> failwith "Cannot find appropriate move"
+ | []    -> failwith "getNewAk: Cannot find appropriate move from an empty list!"
  | x::xs -> let newV = x ^^^ m
             if newV < x then
                 (newV, index)
@@ -31,13 +51,16 @@ let rec getNewAk m index = function
 
 let nextMove (State x) = let m = calculateM 0 x
                          if m = 0 then
-                            let maxi = List.max x
+                            let maxi  = List.max x
                             let index = List.findIndex (fun y -> (y = maxi)) x
                             let array = List.toArray x
                             Array.set array index (maxi-1)
+
+                            printf  "m = 0: newval:%d idx:%d" (maxi-1) index 
                             Array.toList array
                          else 
                             let (newValue, index) = getNewAk m 0 x
+                            printf  "m /= 0: newval:%d idx:%d" newValue index  
                             let array = List.toArray x
                             Array.set array index ((List.nth x index)- newValue)
                             Array.toList array
@@ -46,9 +69,8 @@ let nextMove (State x) = let m = calculateM 0 x
                             
 let reflectMove (state:NimGameState) 
                 (row:int)
-                (col:int) = printf "%s" (string state)
-                            match state with
-                            | State []    -> failwith "Bad state!"
+                (col:int) = match state with
+                            | State []    -> failwith "reflectMove: Bad state (empty)!"
                             | State s -> let listAsArr   = Array.ofList s
                                          let heapSize    = Array.get listAsArr row-1 //Account for index skew.
                                          let newHeapSize = heapSize - (heapSize - (col-1))
@@ -71,21 +93,12 @@ let gameStateFromString (text : string) =
 let gameEvent =  AsyncEventQueue<GameEvent>()
 type NimGame =
   | Nim of NimGameState * NimPlayer
-  member obj.endTurn = match obj with
-                       | Nim (state,Human) -> gameEvent.Post (EndTurn (state,Human))
-                                              Nim (state,AI)
-                       | Nim (state,AI)    -> gameEvent.Post (EndTurn (state,AI))
-                                              Nim (state,Human)
+  //member obj.endTurn = match obj with
+  //                     | Nim (state,player) -> gameEvent.Post (EndTurn (state,player))
   member obj.move row column = match obj with
                                | Nim (state,player) -> //let newState = reflectMove state row column
-                                                      // gameEvent.Post (Move (state)) 
-                                                       Nim (state,AI)
+                                                       gameEvent.Post (Move (row,column))
   static member create intialState = Nim (State intialState, Human)
-
-
-//ng <- ng.endTurn;;
-///ng <- ng.move 1 2;;
-//ng <- ng.endTurn;;
 
 let numberOfHeaps = 5;;
 let matches = [2;3;4;5;6];;
@@ -137,8 +150,7 @@ let urlBox =
 
 let mutable ng = NimGame.create (matches);;
 
-let handleMove (row:int, column:int) = //printf "%d %d \n" row column
-                                       ng <- (ng.move row column)
+let handleMove (row:int, column:int) = gameEvent.Post (Move (row,column))
 
 // Initialization
 let rec generateMatches level = function
@@ -156,7 +168,7 @@ let reduceState matches button = let list = getPosition button
                                  Array.set newArray (index-1) (matches.[index-1] - 1)
                                  State(Array.toList  newArray);;
 let generateButtonMatches matches = let (matchButtons : Control list) = (generateMatches 1 matches)
-                                    ignore(List.map (fun (button : Control) -> button.Click.Add (fun _ -> gameEvent.Post (Move (reduceState matches button)))) matchButtons)
+//                                    ignore(List.map (fun (button : Control) -> button.Click.Add (fun _ -> gameEvent.Post (Move (index button)))) matchButtons)
                                     List.toArray (matchButtons);;
 
 //let disable bs = 
@@ -167,55 +179,66 @@ let generateButtonMatches matches = let (matchButtons : Control list) = (generat
 
 
 
-let rec ready () =
+let rec ready (gameSetup) =
+    let state = gameSetup
     async {
-       printf "Ready: Got event!\n"
+       printf "Ready: Setting up a new game!\n"
        let! msg = gameEvent.Receive()
        match msg with 
-       | EndGame                -> return! finish()
-       | Move (state)         -> return! move(state)
-       | EndTurn (state, player) -> return! nextPlayer(state, player)
-       | StartGame              -> return! setupBoard()
+       | EndGame               -> return! finish()
+       | Move (heap,count)     -> return! move(state, heap, count)
+       | StartGame (gameState) -> return! setupBoard()
     }
 
+// Currently unused..
 and setupBoard() =
     async {
         printf "SetupBoard: Got event!\n"
         let! msg = gameEvent.Receive()
         match msg with 
          | EndGame        -> return! finish()
-         | Move (state)   -> return! move(state)
-         | EndTurn (state, player) -> return! nextPlayer(state, player)
-         | StartGame      -> return! setupBoard()
+         | _              -> failwith "implement me!"
+         //| Move (state)   -> return! move(state)
+         //| EndTurn (state, player) -> return! nextPlayer(state, player)
+         //| StartGame      -> return! setupBoard()
         }
 
-and move (state) =
-    matchPanel.Controls.Clear ()    
-    match state with
+and move (state, heapindex, count) =
+    matchPanel.Controls.Clear ()
+    let newState = reflectMove state heapindex count
+
+    // Update the matches panel with the new state
+    match newState with
       | State [] -> failwith "Bad state"
       | State s  -> matchPanel.Controls.AddRange (generateButtonMatches s)
 
     async {
         printf "Move: Got event!\n"
+        return! AIMove (newState)
+    }
+
+and AIMove (state) =
+    matchPanel.Controls.Clear ()
+    let newIntState = nextMove(state)
+    matchPanel.Controls.AddRange (generateButtonMatches newIntState)
+    async {
+        printf "nextPlayer: Got event!\n"
         let! msg = gameEvent.Receive()
         match msg with 
-         | EndGame        -> return! finish()
-         | Move (state)   -> return! move(state)
-         | EndTurn (state, player) -> return! nextPlayer(state, player)
-         | StartGame      -> return! setupBoard()
+         | EndGame               -> return! finish()
+         | Move (heap,count)     -> return! move(State newIntState, heap, count)
+         | StartGame (gameState) -> return! setupBoard()
         }
-
 and finish () =
     async {
         printf "Finish: Got event!\n"
         let! msg = gameEvent.Receive()
         match msg with 
-         | EndGame        -> return! finish()
-         | Move (state)   -> return! move(state)
-         | EndTurn (state, player) -> return! nextPlayer(state, player)
-         | StartGame      -> return! setupBoard()
+         | EndGame               -> return! finish()
+         | Move (heap,count)     -> failwith "No can do, sir."
+         | StartGame (gameState) -> return! setupBoard()
         }
-
+(*
 and nextPlayer (state, player) =
     matchPanel.Controls.Clear ()
     match player with
@@ -231,7 +254,7 @@ and nextPlayer (state, player) =
          | EndTurn (state, player) -> return! nextPlayer(state, player)
          | StartGame      -> return! setupBoard()
         }
-
+*)
 
 let (buttons : Control []) = generateButtonMatches matches;;
 buttonPanel.Controls.Add startButton
@@ -243,9 +266,11 @@ matchPanel.Controls.AddRange buttons
 window.Controls.Add matchPanel
 window.Controls.Add buttonPanel
 
-startButton.Click.Add (fun _ -> gameEvent.Post (StartGame))
-endTurnButton.Click.Add (fun _ -> ignore (ng.endTurn))
-Async.StartImmediate (ready());
+let initialState = State [2;3;4;5;6]
 
-//Application.Run(window)
-window.Show();;
+startButton.Click.Add (fun _ -> gameEvent.Post (StartGame initialState))
+//endTurnButton.Click.Add (fun _ -> ignore (ng.endTurn))
+Async.StartImmediate (ready(initialState));
+
+Application.Run(window)
+//window.Show();;
