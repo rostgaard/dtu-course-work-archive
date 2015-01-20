5293e2ad257eca83f290e8164ed428cf5844f2fc
Anna Maria Walach <jahi.wh+git@gmail.com>
2015-01-20 10:47:03 +0100

    Refactor - game now support multiple rounds.

--- Project3/NimGame.fsx
+++ Project3/NimGame.fsx
@@ -42,38 +42,35 @@ let toString = function
  | Human -> "Human";;
 
 type NimGameState = State of List<int>
-type GameEvent = Move of int * int | EndGame of NimPlayer| StartGame of NimGameState
+type GameEvent = Move of AsyncEventQueue<GameEvent> * int * int | EndGame of AsyncEventQueue<GameEvent> * NimPlayer 
+                 | StartGame of AsyncEventQueue<GameEvent> * NimGameState
 type NimGameUI = GameUI of List<string> * AsyncEventQueue<GameEvent>;;
 
 
-let gameEvent =  AsyncEventQueue<GameEvent>()
-
+let clickFunction (ev : AsyncEventQueue<GameEvent>) x y = ev.Post (Move (ev, x,y));;
 //GUI elements
-let clickFunction x y = gameEvent.Post (Move (x,y));;
-let matchPanel  = new Panel();;
-let buttonPanel = new Panel();;
-let form = new Form();;
+let mutable matchPanel  = new Panel();;
 
 //game loop helper functions
 let areAllZeroes (state : int list) = List.forall (fun x -> x = 0) state;;
 let simpler (State x) = x;;
 
 //game loop
-let rec ready (gameSetup) =
-    let (buttons : Control []) = NimGUI.generateButtonMatches gameSetup clickFunction
+let rec ready (ev, gameSetup) =
+    let (buttons : Control []) = NimGUI.generateButtonMatches gameSetup (clickFunction ev)
     matchPanel.Controls.AddRange buttons
     matchPanel.BackColor <- Color.Black
     matchPanel.BackgroundImage <- null
     let state = gameSetup
     async {
        printf "Ready: Setting up a new game!\n"
-       let! msg = gameEvent.Receive()
+       let! msg = ev.Receive()
        match msg with 
-       | EndGame player        -> return! finish(player)
-       | Move (heap,count)     -> return! move(state, heap, count)
-       | StartGame (gameState) -> return! ready(gameSetup)
+       | EndGame (ev, player)     -> return! finish(ev, player)
+       | Move (ev, heap,count)     -> return! move(ev, state, heap, count)
+       | StartGame (ev, gameState)-> return! ready (ev, simpler gameState)
     }
-and move (state, heapindex, count) =
+and move (ev, state, heapindex, count) =
     matchPanel.Controls.Clear ()
 
     let newState = (NimGUI.reduceState (state) heapindex count)
@@ -81,57 +78,60 @@ and move (state, heapindex, count) =
     // Update the matches panel with the new state
     match newState with
       | [] -> failwith "Bad state"
-      | s  -> matchPanel.Controls.AddRange (generateButtonMatches s clickFunction)
+      | s  -> matchPanel.Controls.AddRange (generateButtonMatches s (clickFunction ev))
 
     async {
         printf "Move: Got event!\n"
         match newState with 
             | [] -> failwith "Bad state"
             | s -> if areAllZeroes s then
-                    return! finish(Human)
+                    return! finish(ev, Human)
                    else
-                    return! AIMove (newState)
+                    return! AIMove (ev, newState)
     }
 
-and AIMove (state) =
+and AIMove (ev, state) =
     matchPanel.Controls.Clear ()
     let newIntState = NimAI.nextMove state
-    matchPanel.Controls.AddRange (generateButtonMatches newIntState clickFunction)
+    matchPanel.Controls.AddRange (generateButtonMatches newIntState (clickFunction ev))
     async {
         printf "nextPlayer: Got event!\n"
         if areAllZeroes newIntState then
-            return! finish(AI)
+            return! finish(ev, AI)
         else
-            let! msg = gameEvent.Receive()
+            let! msg = ev.Receive()
             match msg with
-              | EndGame  player       -> return! finish(player)
-              | Move (heap,count)     -> return! move(newIntState, heap, count)
-              | StartGame (gameState) -> return! ready(simpler gameState)
+              | EndGame (ev, player)      -> return! finish(ev, player)
+              | Move (ev, heap,count)     -> return! move(ev, newIntState, heap, count)
+              | StartGame (ev, gameState) -> return! ready(ev, simpler gameState)
     }
-and finish (player) =
+and finish (ev, player) =
     async {
         printf "Finish: Got event!\n %s \n" (toString player)
         NimGUI.updateFinishScreen (player <> AI) matchPanel
-        let! msg = gameEvent.Receive()
+        let! msg = ev.Receive()
         match msg with 
-         | EndGame player        -> return! finish(player)
-         | Move (heap,count)     -> failwith "No can do, sir."
-         | StartGame (gameState) -> return! ready(simpler gameState)
+         | EndGame (ev, player)        -> return! finish(ev, player)
+         | Move (ev, heap,count)     -> failwith "No can do, sir."
+         | StartGame (ev, gameState) -> return! ready(ev, simpler gameState)
         }
 
 
 
 //game setup
-let create gameRepr = GameUI (gameRepr, gameEvent);;
+let create gameRepr = GameUI (gameRepr, AsyncEventQueue<GameEvent>());;
 
 let window obj =
     match obj with 
     | GameUI (gameRepr, ev) -> let intList = List.map int gameRepr
+                               let form = new Form()
+                               let buttonPanel = new Panel()
+                               matchPanel <- new Panel()
                                NimGUI.updatePanels intList matchPanel buttonPanel form
                                let resetBtn = NimGUI.resetBtn buttonPanel
                                form.Controls.Add matchPanel
                                form.Controls.Add buttonPanel
                                buttonPanel.Controls.Add resetBtn
-                               resetBtn.Click.Add (fun _ -> gameEvent.Post (StartGame (State intList)))
-                               Async.StartImmediate (ready intList)
+                               resetBtn.Click.Add (fun _ -> ev.Post (StartGame (ev, State intList)))
+                               Async.StartImmediate (ready (ev, intList))
                                form
